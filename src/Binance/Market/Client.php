7f26bcd738e2a0ebe169347308836a5a0034f53c
lianbo.wan@gmail.com <wanlianbo>
2021-02-06 09:36:15 +0800

    add trades historicalTrades aggTrades api

--- src/Binance/Market/Client.php
+++ src/Binance/Market/Client.php
@@ -10,4 +10,33 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v3/depth', compact('symbol', 'limit'));
     }
+
+    public function trades($symbol, int $limit = 500)
+    {
+        return $this->httpGet('/api/v3/trades', compact('symbol', 'limit'));
+    }
+
+    public function historicalTrades($symbol, $fromId = '', int $limit = 500)
+    {
+        return $this->httpGet('/api/v3/historicalTrades', compact('symbol', 'fromId', 'limit'));
+    }
+
+    public function aggTrades($symbol, $fromId = '', $startTime = '', $endTime = '', int $limit = 50)
+    {
+        $request = ['symbol' => $symbol];
+        if ($fromId) {
+            $request['fromId'] = $fromId;
+        }
+        if ($startTime) {
+            $request['startTime'] = $startTime;
+        }
+        if ($endTime) {
+            $request['endTime'] = $endTime;
+        }
+        if ($limit) {
+            $request['limit'] = $limit;
+        }
+
+        return $this->httpGet('/api/v3/aggTrades', $request);
+    }
 }
