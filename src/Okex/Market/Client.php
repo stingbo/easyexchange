801e0d1d39dd6979d7fc6350530a6e28a08063e8
sting <sting_bo@163.com>
2021-02-12 19:06:39 +0800

    add okex depth api

--- src/Okex/Market/Client.php
+++ src/Okex/Market/Client.php
@@ -6,4 +6,8 @@ use EasyExchange\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    public function depth($instId, $sz = 1)
+    {
+        return $this->httpGet('/api/v5/market/books', compact('instId', 'sz'));
+    }
 }
