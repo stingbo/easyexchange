56108afffe42a6f9d86db80c45b74975f91f3e4d
sting <sting_bo@163.com>
2021-02-13 23:45:10 +0800

    order api

--- src/Binance/Trade/Client.php
+++ src/Binance/Trade/Client.php
@@ -33,6 +33,13 @@ class Client extends BaseClient
      */
     public function order($params)
     {
+        $params['timestamp'] = $this->getMs();
+        ksort($params);
+        $data = http_build_query($params);
+        $secret = $this->app->config->get('secret');
+        $signature = hash_hmac('SHA256', $data, $secret);
+        $params['signature'] = $signature;
+
         return $this->httpPost('/api/v3/order', $params);
     }
 }
