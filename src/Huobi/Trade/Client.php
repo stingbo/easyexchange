63a0b284e4b0750ebd42b83f9889285cf2877e28
sting <sting_bo@163.com>
2021-02-14 23:28:43 +0800

    add huobi openOrders api

--- src/Huobi/Trade/Client.php
+++ src/Huobi/Trade/Client.php
@@ -6,4 +6,26 @@ use EasyExchange\Huobi\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    public function order($params)
+    {
+    }
+
+    public function account()
+    {
+    }
+
+    /**
+     * 查询当前未成交订单.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function openOrders($params)
+    {
+        return $this->httpGet('/v1/order/openOrders', $params, 'TRADE');
+    }
 }
