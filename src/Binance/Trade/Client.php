0f7ff480a31f421ef89f99d1e07b79d49dfd3d15
sting <sting_bo@163.com>
2021-02-15 11:44:37 +0800

    add binance cancel order api

--- src/Binance/Trade/Client.php
+++ src/Binance/Trade/Client.php
@@ -66,4 +66,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v3/openOrders', $params, 'TRADE');
     }
+
+    /**
+     * 撤销订单.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function cancelOrder($params)
+    {
+        return $this->httpDelete('/api/v3/order', $params, 'TRADE');
+    }
 }
