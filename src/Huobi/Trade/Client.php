aaa44bf67ad084f8f38280bed78f501696a26d7e
sting <sting_bo@163.com>
2021-02-15 11:03:06 +0800

    complete order and accounts api

--- src/Huobi/Trade/Client.php
+++ src/Huobi/Trade/Client.php
@@ -6,12 +6,32 @@ use EasyExchange\Huobi\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * new order.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function order($params)
     {
+        return $this->httpPostJson('/v1/order/orders/place', $params, [], 'TRADE');
     }
 
-    public function account()
+    /**
+     * Get all Accounts of the Current User.
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function accounts()
     {
+        return $this->httpGet('/v1/account/accounts', [], 'TRADE');
     }
 
     /**
