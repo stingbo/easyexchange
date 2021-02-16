b63b923f3623c87d45e54d517f53cf84d112f8b2
sting <sting_bo@163.com>
2021-02-16 23:13:31 +0800

    move ping systemTime exchangeInfo to basic

--- src/Binance/Market/Client.php
+++ src/Binance/Market/Client.php
@@ -7,45 +7,6 @@ use EasyExchange\Binance\Kernel\BaseClient;
 class Client extends BaseClient
 {
     /**
-     * 测试服务器连通性.
-     *
-     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
-     *
-     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function ping()
-    {
-        return $this->httpGet('/api/v3/ping');
-    }
-
-    /**
-     * 获取服务器时间.
-     *
-     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
-     *
-     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function systemTime()
-    {
-        return $this->httpGet('/api/v3/time');
-    }
-
-    /**
-     * 交易规范信息.
-     *
-     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
-     *
-     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function exchangeInfo()
-    {
-        return $this->httpGet('/api/v3/exchangeInfo');
-    }
-
-    /**
      * 深度信息.
      *
      * @param $symbol
