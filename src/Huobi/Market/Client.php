572594b31225d089a0263c089a7b99397a21f542
sting <sting_bo@163.com>
2021-02-16 23:48:20 +0800

    add huobi hr24 api

--- src/Huobi/Market/Client.php
+++ src/Huobi/Market/Client.php
@@ -80,4 +80,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/market/detail/merged', compact('symbol'));
     }
+
+    /**
+     * 最近24小时行情数据.
+     *
+     * @param $symbol
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function hr24($symbol)
+    {
+        return $this->httpGet('/market/detail', compact('symbol'));
+    }
 }
