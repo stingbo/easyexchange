28e738f020d94dd21afaa6e3eb4409ed67329ec7
sting <sting_bo@163.com>
2021-02-16 22:33:44 +0800

    add systemTime api

--- src/Binance/Market/Client.php
+++ src/Binance/Market/Client.php
@@ -20,6 +20,19 @@ class Client extends BaseClient
     }
 
     /**
+     * 获取服务器时间.
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function systemTime()
+    {
+        return $this->httpGet('/api/v3/time');
+    }
+
+    /**
      * 交易规范信息.
      *
      * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
