60f9d481ee0f9ed15c22834e9cbca69f7cab6416
sting <sting_bo@163.com>
2021-02-17 12:28:16 +0800

    move account api to wallet client

--- src/Binance/Trade/Client.php
+++ src/Binance/Trade/Client.php
@@ -37,21 +37,6 @@ class Client extends BaseClient
     }
 
     /**
-     * 账户信息.
-     *
-     * @param int $recvWindow
-     *
-     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
-     *
-     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function account($recvWindow = 60000)
-    {
-        return $this->httpGet('/api/v3/account', compact('recvWindow'), 'TRADE');
-    }
-
-    /**
      * 获取交易对的所有当前挂单.
      *
      * @param string $symbol
