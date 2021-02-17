dedf06b58e10ff4234c8cdfabaf8df2dcc7ae3fa
sting <sting_bo@163.com>
2021-02-17 12:55:57 +0800

    add huobi assetValuation 按照BTC或法币计价单位，获取指定账户的总资产估值 api

--- src/Huobi/Wallet/Client.php
+++ src/Huobi/Wallet/Client.php
@@ -33,4 +33,21 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/v1/account/accounts/%s/balance', $account_id), [], 'TRADE');
     }
+
+    /**
+     * 按照BTC或法币计价单位，获取指定账户的总资产估值.
+     *
+     * @param string $accountType
+     * @param string $valuationCurrency
+     * @param string $subUid
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function assetValuation($accountType = 'spot', $valuationCurrency = 'BTC', $subUid = '')
+    {
+        return $this->httpGet('/v2/account/asset-valuation', compact('accountType', 'valuationCurrency', 'subUid'), 'TRADE');
+    }
 }
