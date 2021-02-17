9770847d3fc52c237fd6e5dc81b979d2adbd40e0
sting <sting_bo@163.com>
2021-02-17 13:53:58 +0800

    add houbi history 账户流水 api

--- src/Huobi/Wallet/Client.php
+++ src/Huobi/Wallet/Client.php
@@ -65,4 +65,19 @@ class Client extends BaseClient
     {
         return $this->httpPost('/v1/account/transfer', $params, 'TRADE');
     }
+
+    /**
+     * 账户流水.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function history($params)
+    {
+        return $this->httpGet('/v1/account/history', $params, 'TRADE');
+    }
 }
