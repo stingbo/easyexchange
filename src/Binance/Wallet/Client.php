213f9059b76a66b9e89e3b077de477e3326454f6
sting <sting_bo@163.com>
2021-02-17 12:28:32 +0800

    add binance accountSnapshot 查询每日资产快照 api

--- src/Binance/Wallet/Client.php
+++ src/Binance/Wallet/Client.php
@@ -7,6 +7,21 @@ use EasyExchange\Binance\Kernel\BaseClient;
 class Client extends BaseClient
 {
     /**
+     * 账户信息.
+     *
+     * @param int $recvWindow
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function account($recvWindow = 60000)
+    {
+        return $this->httpGet('/api/v3/account', compact('recvWindow'), 'TRADE');
+    }
+
+    /**
      * 获取所有币信息.
      * No sapi/wapi in testnet; only api endpoints available.
      *
@@ -18,5 +33,20 @@ class Client extends BaseClient
     public function getAll()
     {
         return $this->httpGet('/sapi/v1/capital/config/getall', [], 'TRADE');
+    }
+
+    /**
+     * 查询每日资产快照.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function accountSnapshot($params)
+    {
+        return $this->httpGet('/sapi/v1/accountSnapshot', $params, 'TRADE');
     }
 }
