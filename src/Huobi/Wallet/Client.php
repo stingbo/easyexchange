bc603a527ab702c24af2bb16988f7259f05bc0ce
sting <sting_bo@163.com>
2021-02-17 12:46:15 +0800

    add huobi account 账户余额 api

--- src/Huobi/Wallet/Client.php
+++ src/Huobi/Wallet/Client.php
@@ -18,4 +18,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/v1/account/accounts', [], 'TRADE');
     }
+
+    /**
+     * 账户余额.
+     *
+     * @param $account_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function account($account_id)
+    {
+        return $this->httpGet(sprintf('/v1/account/accounts/%s/balance', $account_id), [], 'TRADE');
+    }
 }
