c5275ff815a4f79895b211f423e611db444b2728
sting <sting_bo@163.com>
2021-02-17 09:47:21 +0800

    add binance kline api

--- src/Binance/Market/Client.php
+++ src/Binance/Market/Client.php
@@ -81,4 +81,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v3/ticker/24hr', compact('symbol'));
     }
+
+    /**
+     * K线数据.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function kline($params)
+    {
+        return $this->httpGet('/api/v3/klines', $params);
+    }
 }
