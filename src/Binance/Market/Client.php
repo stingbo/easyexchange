0fcae636237e430e1627b190f8517842921d7674
sting <sting_bo@163.com>
2021-02-17 09:48:23 +0800

    add binance avgPrice api

--- src/Binance/Market/Client.php
+++ src/Binance/Market/Client.php
@@ -96,4 +96,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v3/klines', $params);
     }
+
+    /**
+     * 当前平均价格.
+     *
+     * @param $symbol
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function avgPrice($symbol)
+    {
+        return $this->httpGet('/api/v3/avgPrice', compact('symbol'));
+    }
 }
