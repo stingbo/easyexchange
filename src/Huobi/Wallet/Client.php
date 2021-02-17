674c632d6d26da42117aed49bf68b13312f4dbe6
sting <sting_bo@163.com>
2021-02-17 12:42:04 +0800

    move accounts from src/Huobi/Trade/Client to src/Huobi/Wallet/Client

--- src/Huobi/Wallet/Client.php
+++ src/Huobi/Wallet/Client.php
@@ -6,4 +6,16 @@ use EasyExchange\Huobi\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * Get all Accounts of the Current User.
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function accounts()
+    {
+        return $this->httpGet('/v1/account/accounts', [], 'TRADE');
+    }
 }
