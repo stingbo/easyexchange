7313383c67a87f9ea80d8f25879609b7e6239648
sting <sting_bo@163.com>
2021-02-25 09:03:06 +0800

    add binance getOcoOrder 查询 OCO api

--- src/Binance/Trade/Client.php
+++ src/Binance/Trade/Client.php
@@ -165,4 +165,19 @@ class Client extends BaseClient
     {
         return $this->httpDelete('/api/v3/orderList', $params, 'TRADE');
     }
+
+    /**
+     * 查询 OCO.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getOcoOrder($params)
+    {
+        return $this->httpGet('/api/v3/orderList', $params, 'TRADE');
+    }
 }
