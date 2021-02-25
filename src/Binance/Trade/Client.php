990e5c5163ce6613cc517910e552be8fd7890a0d
sting <sting_bo@163.com>
2021-02-25 09:01:20 +0800

    add binance cancelOcoOrder api

--- src/Binance/Trade/Client.php
+++ src/Binance/Trade/Client.php
@@ -150,4 +150,19 @@ class Client extends BaseClient
     {
         return $this->httpPost('/api/v3/order/oco', $params, 'TRADE');
     }
+
+    /**
+     * 取消 OCO 订单.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function cancelOcoOrder($params)
+    {
+        return $this->httpDelete('/api/v3/orderList', $params, 'TRADE');
+    }
 }
