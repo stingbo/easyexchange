ac9886ed282f9299e266379dfdd66311852fe066
sting <sting_bo@163.com>
2021-02-25 09:00:54 +0800

    add binance oco api

--- src/Binance/Trade/Client.php
+++ src/Binance/Trade/Client.php
@@ -135,4 +135,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v3/myTrades', $params, 'TRADE');
     }
+
+    /**
+     * OCO下单.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function oco($params)
+    {
+        return $this->httpPost('/api/v3/order/oco', $params, 'TRADE');
+    }
 }
