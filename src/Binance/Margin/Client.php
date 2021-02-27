9161a2c7a54cf4f5f7c9404c6a4e37882eb07d52
sting <sting_bo@163.com>
2021-02-27 23:29:19 +0800

    add binance transfer 全仓杠杆账户划转 (MARGIN) api

--- src/Binance/Margin/Client.php
+++ src/Binance/Margin/Client.php
@@ -6,4 +6,18 @@ use EasyExchange\Binance\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * 全仓杠杆账户划转 (MARGIN).
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function transfer($params)
+    {
+        return $this->httpPost('/sapi/v1/margin/transfer', $params, 'TRADE');
+    }
 }
