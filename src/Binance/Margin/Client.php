54d6b8e29236cd92e67cd4775b8c1ef16d08fa7a
sting <sting_bo@163.com>
2021-02-28 12:09:34 +0800

    add binance /sapi/v1/margin/allOrders 查询杠杆账户的所有订单. api

--- src/Binance/Margin/Client.php
+++ src/Binance/Margin/Client.php
@@ -286,4 +286,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/sapi/v1/margin/openOrders', $params, 'SIGN');
     }
+
+    /**
+     * 查询杠杆账户的所有订单.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function allOrders($params)
+    {
+        return $this->httpGet('/sapi/v1/margin/allOrders', $params, 'SIGN');
+    }
 }
