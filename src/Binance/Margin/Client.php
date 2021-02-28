da0dc19b1ae6eb89d3b78e4d2d726fb1a6dd1a61
sting <sting_bo@163.com>
2021-02-28 11:53:02 +0800

    add binance /sapi/v1/margin/order 杠杆账户撤销订单

--- src/Binance/Margin/Client.php
+++ src/Binance/Margin/Client.php
@@ -136,4 +136,19 @@ class Client extends BaseClient
     {
         return $this->httpPost('/sapi/v1/margin/order', $params, 'SIGN');
     }
+
+    /**
+     * 杠杆账户撤销订单.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function cancelOrder($params)
+    {
+        return $this->httpDelete('/sapi/v1/margin/order', $params, 'SIGN');
+    }
 }
