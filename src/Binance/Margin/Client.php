cd7d1d5fec7e3866d6a1e6285928e8e177dba513
sting <sting_bo@163.com>
2021-02-28 11:50:39 +0800

    update /sapi/v1/margin/priceIndex api

--- src/Binance/Margin/Client.php
+++ src/Binance/Margin/Client.php
@@ -110,14 +110,16 @@ class Client extends BaseClient
     /**
      * 查询杠杆价格指数.
      *
+     * @param $symbol
+     *
      * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
      *
      * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function priceIndex()
+    public function priceIndex($symbol)
     {
-        return $this->httpGet('/sapi/v1/margin/priceIndex', [], 'APIKEY');
+        return $this->httpGet('/sapi/v1/margin/priceIndex', compact('symbol'), 'APIKEY');
     }
 
     /**
