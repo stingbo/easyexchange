e6847605dfabbfed325669a8cf8409233d016596
sting <sting_bo@163.com>
2021-02-28 12:12:27 +0800

    add binance /sapi/v1/margin/maxTransferable 查询最大可转出额. api

--- src/Binance/Margin/Client.php
+++ src/Binance/Margin/Client.php
@@ -331,4 +331,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/sapi/v1/margin/maxBorrowable', $params, 'SIGN');
     }
+
+    /**
+     * 查询最大可转出额.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function maxTransferable($params)
+    {
+        return $this->httpGet('/sapi/v1/margin/maxTransferable', $params, 'SIGN');
+    }
 }
