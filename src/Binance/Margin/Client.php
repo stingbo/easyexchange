a1df0ecc92d777d2a2b152dbb6680fca1c6d828b
sting <sting_bo@163.com>
2021-02-28 10:59:58 +0800

    add binance /sapi/v1/margin/asset 查询杠杆资产 api

--- src/Binance/Margin/Client.php
+++ src/Binance/Margin/Client.php
@@ -50,4 +50,19 @@ class Client extends BaseClient
     {
         return $this->httpPost('/sapi/v1/margin/repay', $params, 'TRADE');
     }
+
+    /**
+     * 查询杠杆资产.
+     *
+     * @param $asset
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function asset($asset)
+    {
+        return $this->httpPost('/sapi/v1/margin/asset', compact('asset'), 'TRADE');
+    }
 }
