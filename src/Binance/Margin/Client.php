6c41bbeacf3916750806895603d719284ef3d99e
sting <sting_bo@163.com>
2021-02-28 10:50:02 +0800

    add binance /sapi/v1/margin/loan 杠杆账户借贷 api

--- src/Binance/Margin/Client.php
+++ src/Binance/Margin/Client.php
@@ -7,7 +7,7 @@ use EasyExchange\Binance\Kernel\BaseClient;
 class Client extends BaseClient
 {
     /**
-     * 全仓杠杆账户划转 (MARGIN).
+     * 全仓杠杆账户划转.
      *
      * @param $params
      *
@@ -19,5 +19,20 @@ class Client extends BaseClient
     public function transfer($params)
     {
         return $this->httpPost('/sapi/v1/margin/transfer', $params, 'TRADE');
+    }
+
+    /**
+     * 杠杆账户借贷.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function loan($params)
+    {
+        return $this->httpPost('/sapi/v1/margin/loan', $params, 'TRADE');
     }
 }
