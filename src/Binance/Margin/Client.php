0daf1c4380f55dfff04418ca51487e067b971660
sting <sting_bo@163.com>
2021-02-28 12:02:37 +0800

    add binance /sapi/v1/margin/interestHistory 获取利息历史.

--- src/Binance/Margin/Client.php
+++ src/Binance/Margin/Client.php
@@ -211,4 +211,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/sapi/v1/margin/repay', $params, 'SIGN');
     }
+
+    /**
+     * 获取利息历史.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function interestHistory($params)
+    {
+        return $this->httpGet('/sapi/v1/margin/interestHistory', $params, 'SIGN');
+    }
 }
