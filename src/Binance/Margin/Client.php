44547936895fbf60485998ebddbf17b5a1e142e2
sting <sting_bo@163.com>
2021-02-28 11:59:53 +0800

    add binance /sapi/v1/margin/transfer 获取全仓杠杆划转历史. api

--- src/Binance/Margin/Client.php
+++ src/Binance/Margin/Client.php
@@ -166,4 +166,19 @@ class Client extends BaseClient
     {
         return $this->httpDelete('/sapi/v1/margin/openOrders', $params, 'SIGN');
     }
+
+    /**
+     * 获取全仓杠杆划转历史.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function transferHistory($params)
+    {
+        return $this->httpGet('/sapi/v1/margin/transfer', $params, 'SIGN');
+    }
 }
