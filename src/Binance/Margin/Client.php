b358b212631939a307868f33e8adc3a51f878b8d
sting <sting_bo@163.com>
2021-02-28 12:00:49 +0800

    add binance /sapi/v1/margin/loan 查询借贷记录.

--- src/Binance/Margin/Client.php
+++ src/Binance/Margin/Client.php
@@ -181,4 +181,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/sapi/v1/margin/transfer', $params, 'SIGN');
     }
+
+    /**
+     * 查询借贷记录.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function loanHistory($params)
+    {
+        return $this->httpGet('/sapi/v1/margin/loan', $params, 'SIGN');
+    }
 }
