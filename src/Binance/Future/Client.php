3bd0ddcd0f000e6dcc95e422f9cdce02b3a1e4c3
sting <sting_bo@163.com>
2021-03-02 09:00:31 +0800

    add binance /sapi/v1/futures/transfer 合约资金划转 api

--- src/Binance/Future/Client.php
+++ src/Binance/Future/Client.php
@@ -6,4 +6,18 @@ use EasyExchange\Binance\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * 合约资金划转.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function transfer($params)
+    {
+        return $this->httpPost('/sapi/v1/futures/transfer', $params, 'SIGN');
+    }
 }
