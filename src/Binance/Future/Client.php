0946a6dd80a6e068b0bdc5bd48d0dfd5427354aa
sting <sting_bo@163.com>
2021-03-03 08:52:03 +0800

    add binanace /sapi/v1/futures/loan/borrow 混合保证金借款 api

--- src/Binance/Future/Client.php
+++ src/Binance/Future/Client.php
@@ -35,4 +35,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/sapi/v1/futures/transfer', $params, 'SIGN');
     }
+
+    /**
+     * 混合保证金借款.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function borrow($params)
+    {
+        return $this->httpPost('/sapi/v1/futures/loan/borrow', $params, 'SIGN');
+    }
 }
