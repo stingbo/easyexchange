cb2d695bc5140b1f15d78f7be5c7d50ede263afa
wanlianbo <lianbo.wan@gmail.com>
2021-03-03 21:56:05 +0800

    add binance /sapi/%s/futures/loan/calcMaxAdjustAmount 可供调整混合保证金质押率的最大额 v1 & v2 api

--- src/Binance/Future/Client.php
+++ src/Binance/Future/Client.php
@@ -143,4 +143,20 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/sapi/%v/futures/loan/calcAdjustLevel', $version), $params, 'SIGN');
     }
+
+    /**
+     * 可供调整混合保证金质押率的最大额 v1 & v2.
+     *
+     * @param $params
+     * @param string $version
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function calcMaxAdjustAmount($params, $version = 'v1')
+    {
+        return $this->httpGet(sprintf('/sapi/%s/futures/loan/calcMaxAdjustAmount', $version), $params, 'SIGN');
+    }
 }
