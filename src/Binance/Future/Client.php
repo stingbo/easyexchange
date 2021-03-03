88fbdd63079f0ffd60e092be4a6b6a94cd05b085
sting <sting_bo@163.com>
2021-03-03 08:54:35 +0800

    add binance /sapi/v1/futures/loan/borrow/history 混合保证金借款历史 api

--- src/Binance/Future/Client.php
+++ src/Binance/Future/Client.php
@@ -50,4 +50,19 @@ class Client extends BaseClient
     {
         return $this->httpPost('/sapi/v1/futures/loan/borrow', $params, 'SIGN');
     }
+
+    /**
+     * 混合保证金借款历史.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function borrowHistory($params)
+    {
+        return $this->httpGet('/sapi/v1/futures/loan/borrow/history', $params, 'SIGN');
+    }
 }
