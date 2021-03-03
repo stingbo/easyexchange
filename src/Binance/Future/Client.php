38351232e16f4566d0d8ee1ac5826185c79cf7d4
wanlianbo <lianbo.wan@gmail.com>
2021-03-03 22:13:09 +0800

    add binance /sapi/v1/futures/loan/interestHistory 混合保证金利息收取历史 api

--- src/Binance/Future/Client.php
+++ src/Binance/Future/Client.php
@@ -266,4 +266,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/sapi/v1/futures/loan/collateralRepayResult', compact('quoteId', 'recvWindow'), 'SIGN');
     }
+
+    /**
+     * 混合保证金利息收取历史.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function interestHistory($params)
+    {
+        return $this->httpGet('/sapi/v1/futures/loan/interestHistory', $params, 'SIGN');
+    }
 }
