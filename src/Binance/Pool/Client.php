1145b86fcfbf3883684445ffe99b2204776d80ae
wanlianbo <lianbo.wan@gmail.com>
2021-03-04 22:39:02 +0800

    add binance /sapi/v1/mining/statistics/user/status 统计列表 api

--- src/Binance/Pool/Client.php
+++ src/Binance/Pool/Client.php
@@ -170,4 +170,19 @@ class Client extends BaseClient
     {
         return $this->httpPost('/sapi/v1/mining/hash-transfer/config/cancel', $params, 'SIGN');
     }
+
+    /**
+     * 统计列表.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function userStatus($params)
+    {
+        return $this->httpGet('/sapi/v1/mining/statistics/user/status', $params, 'SIGN');
+    }
 }
