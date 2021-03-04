c68785c5c68b04240c87275a945b6419caac2118
wanlianbo <lianbo.wan@gmail.com>
2021-03-04 22:19:05 +0800

    add binance /sapi/v1/mining/worker/list 请求矿工列表 api

--- src/Binance/Pool/Client.php
+++ src/Binance/Pool/Client.php
@@ -50,4 +50,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/sapi/v1/mining/worker/detail', $params, 'SIGN');
     }
+
+    /**
+     * 请求矿工列表.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function workerList($params)
+    {
+        return $this->httpGet('/sapi/v1/mining/worker/list', $params, 'SIGN');
+    }
 }
