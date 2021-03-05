b6fbedfdfb6df3d5ecdc2bccf896eae77a4de0f0
sting <sting_bo@163.com>
2021-03-05 09:45:48 +0800

    add binance /sapi/v1/asset/transfer 查询用户万向划转历史 api

--- src/Binance/Wallet/Client.php
+++ src/Binance/Wallet/Client.php
@@ -329,4 +329,19 @@ class Client extends BaseClient
     {
         return $this->httpPost('/sapi/v1/asset/transfer', $params, 'SIGN');
     }
+
+    /**
+     * 查询用户万向划转历史.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function transferHistory($params)
+    {
+        return $this->httpGet('/sapi/v1/asset/transfer', $params, 'SIGN');
+    }
 }
