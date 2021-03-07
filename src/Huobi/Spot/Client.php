6d8351935973c1e0376813bf8bfc47a272b22b7e
sting <sting_bo@163.com>
2021-03-07 19:02:57 +0800

    add huobi /v2/algo-orders/cancel-all-after 自动撤销订单 api

--- src/Huobi/Spot/Client.php
+++ src/Huobi/Spot/Client.php
@@ -80,4 +80,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/v1/order/orders/submitCancelClientOrder', compact('client_order_id'), [], 'SIGN');
     }
+
+    /**
+     * 自动撤销订单.
+     *
+     * @param $timeout
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function cancelAllAfter($timeout)
+    {
+        return $this->httpPostJson('/v2/algo-orders/cancel-all-after', compact('timeout'), [], 'SIGN');
+    }
 }
