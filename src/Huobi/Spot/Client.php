92200d8b9ed6bafe0a8ea6e89beb6cf934ec1288
sting <sting_bo@163.com>
2021-03-07 20:36:11 +0800

    add huobi /v2/reference/transact-fee-rate 获取用户当前手续费率 api

--- src/Huobi/Spot/Client.php
+++ src/Huobi/Spot/Client.php
@@ -216,4 +216,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/v1/order/matchresults', $params, 'SIGN');
     }
+
+    /**
+     * 获取用户当前手续费率.
+     *
+     * @param $symbols
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function transactFeeRate($symbols)
+    {
+        return $this->httpGet('/v2/reference/transact-fee-rate', compact('symbols'), 'SIGN');
+    }
 }
