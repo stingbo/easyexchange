6d6ffe65e0b2d194214a91697c30d6ec2a1e0284
sting <sting_bo@163.com>
2021-03-07 19:45:01 +0800

    add huobi /v1/order/orders/%s/matchresults 成交明细 api

--- src/Huobi/Spot/Client.php
+++ src/Huobi/Spot/Client.php
@@ -156,4 +156,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/v1/order/orders/getClientOrder', compact('client_order_id'), 'SIGN');
     }
+
+    /**
+     * 成交明细.
+     *
+     * @param $order_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function matchResults($order_id)
+    {
+        return $this->httpGet(sprintf('/v1/order/orders/%s/matchresults', $order_id), [], 'SIGN');
+    }
 }
