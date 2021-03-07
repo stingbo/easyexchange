644f7cafca17c6f624c73cab21449238ca6965bc
sting <sting_bo@163.com>
2021-03-07 19:34:00 +0800

    add huobi /v1/order/orders/getClientOrder 查询订单详情（基于client order ID） api

--- src/Huobi/Spot/Client.php
+++ src/Huobi/Spot/Client.php
@@ -141,4 +141,19 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/v1/order/orders/%s', $order_id), [], 'SIGN');
     }
+
+    /**
+     * 查询订单详情（基于client order ID）.
+     *
+     * @param $client_order_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getClientOrder($client_order_id)
+    {
+        return $this->httpGet('/v1/order/orders/getClientOrder', compact('client_order_id'), 'SIGN');
+    }
 }
