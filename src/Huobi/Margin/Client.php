4932fdefe01c876e972885db86f770d6593dde94
sting <sting_bo@163.com>
2021-03-08 08:58:45 +0800

    add huobi /v1/margin/loan-info 查询借币币息率及额度（逐仓）api

--- src/Huobi/Margin/Client.php
+++ src/Huobi/Margin/Client.php
@@ -50,4 +50,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/v1/dw/transfer-out/margin', $params, [], 'SIGN');
     }
+
+    /**
+     * 查询借币币息率及额度（逐仓）.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function loanInfo($params)
+    {
+        return $this->httpGet('/v1/margin/loan-info', $params, 'SIGN');
+    }
 }
