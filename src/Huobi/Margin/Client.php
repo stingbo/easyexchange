236506621079d61e3d66bc5367eb413895758fd1
sting <sting_bo@163.com>
2021-03-08 08:57:40 +0800

    add huobi /v1/dw/transfer-in/margin 资产划转（逐仓）-从现货账户划转至逐仓杠杆账户 api

--- src/Huobi/Margin/Client.php
+++ src/Huobi/Margin/Client.php
@@ -20,4 +20,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/v2/account/repayment', $params, [], 'SIGN');
     }
+
+    /**
+     * 资产划转（逐仓）-从现货账户划转至逐仓杠杆账户.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function transferIn($params)
+    {
+        return $this->httpPostJson('/v1/dw/transfer-in/margin', $params, [], 'SIGN');
+    }
 }
