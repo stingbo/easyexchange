ba994425de778ba0a8f9cc4b50503a5398b81afa
sting <sting_bo@163.com>
2021-03-09 08:59:14 +0800

    add huobi /v1/margin/orders/%s/repay 归还借币（逐仓）

--- src/Huobi/Margin/Client.php
+++ src/Huobi/Margin/Client.php
@@ -80,4 +80,20 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/v1/margin/orders', $params, [], 'SIGN');
     }
+
+    /**
+     * 归还借币（逐仓）.
+     *
+     * @param $order_id
+     * @param $amount
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function repay($order_id, $amount)
+    {
+        return $this->httpPostJson(sprintf('/v1/margin/orders/%s/repay', $order_id), compact('amount'), [], 'SIGN');
+    }
 }
