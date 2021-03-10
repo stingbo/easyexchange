8faa25e640fb011eb4620432ebd29821f70b1181
sting <sting_bo@163.com>
2021-03-10 09:04:18 +0800

    add huobi /v1/cross-margin/orders/%s/repay 归还借币（全仓） api

--- src/Huobi/Margin/Client.php
+++ src/Huobi/Margin/Client.php
@@ -196,4 +196,20 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/v1/cross-margin/orders', compact('currency', 'amount'), [], 'SIGN');
     }
+
+    /**
+     * 归还借币（全仓）.
+     *
+     * @param $order_id
+     * @param $amount
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function crossRepay($order_id, $amount)
+    {
+        return $this->httpPostJson(sprintf('/v1/cross-margin/orders/%s/repay', $order_id), compact('amount'), [], 'SIGN');
+    }
 }
