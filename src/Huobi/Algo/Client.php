adbd5a259cf0a5b2ead40245f8a68818487c12d6
sting <sting_bo@163.com>
2021-03-11 09:04:58 +0800

    add huobi /v2/algo-orders 策略委托下单 api

--- src/Huobi/Algo/Client.php
+++ src/Huobi/Algo/Client.php
@@ -6,4 +6,18 @@ use EasyExchange\Huobi\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * 策略委托下单.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function order($params)
+    {
+        return $this->httpPostJson(' /v2/algo-orders', $params, [], 'SIGN');
+    }
 }
