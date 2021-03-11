4e86efd022cc23c344a7aac277ffddf025257139
sting <sting_bo@163.com>
2021-03-11 09:10:32 +0800

    add huobi /v2/algo-orders/history 查询策略委托历史 api

--- src/Huobi/Algo/Client.php
+++ src/Huobi/Algo/Client.php
@@ -50,4 +50,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/v2/algo-orders/opening', $params, 'SIGN');
     }
+
+    /**
+     * 查询策略委托历史.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function history($params)
+    {
+        return $this->httpGet('/v2/algo-orders/history', $params, 'SIGN');
+    }
 }
