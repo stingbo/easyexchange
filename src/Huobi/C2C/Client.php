b868cf6ebfcb80c547b5d9002a711d36bf5c03db
sting <sting_bo@163.com>
2021-03-12 09:10:03 +0800

    add huobi /v2/c2c/offer 借入借出下单 api

--- src/Huobi/C2C/Client.php
+++ src/Huobi/C2C/Client.php
@@ -6,4 +6,18 @@ use EasyExchange\Huobi\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * 借入借出下单.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function offer($params)
+    {
+        return $this->httpPostJson('/v2/c2c/offer', $params, [], 'SIGN');
+    }
 }
