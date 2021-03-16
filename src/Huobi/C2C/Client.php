5b3fbc4a477de9549dc832d008fbb5a09c65ac07
wanlianbo <lianbo.wan@gmail.com>
2021-03-16 23:09:31 +0800

    add huobi /v2/c2c/transactions 查询借入借出交易记录 api

--- src/Huobi/C2C/Client.php
+++ src/Huobi/C2C/Client.php
@@ -80,4 +80,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/v2/c2c/offer', compact('offerId'), 'SIGN');
     }
+
+    /**
+     * 查询借入借出交易记录.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function transactions($params)
+    {
+        return $this->httpGet('/v2/c2c/transactions', $params, 'SIGN');
+    }
 }
