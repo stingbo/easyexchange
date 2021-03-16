03d26781eb74659f5435cdfaabd98ce930faf09b
wanlianbo <lianbo.wan@gmail.com>
2021-03-16 22:48:31 +0800

    add huobi /v2/c2c/cancel-all 撤销所有借入借出订单 api

--- src/Huobi/C2C/Client.php
+++ src/Huobi/C2C/Client.php
@@ -35,4 +35,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/v2/c2c/cancellation', $params, [], 'SIGN');
     }
+
+    /**
+     * 撤销所有借入借出订单.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function cancelAll($params)
+    {
+        return $this->httpPostJson('/v2/c2c/cancel-all', $params, [], 'SIGN');
+    }
 }
