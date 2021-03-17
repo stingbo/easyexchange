f6c3f2a9c417785149df43e521dc354dfad80477
sting <sting_bo@163.com>
2021-03-17 08:50:57 +0800

    add huobi /v2/c2c/repayment 查询还币交易记录 api

--- src/Huobi/C2C/Client.php
+++ src/Huobi/C2C/Client.php
@@ -110,4 +110,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/v2/c2c/repayment', $params, [], 'SIGN');
     }
+
+    /**
+     * 查询还币交易记录.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getRepayment($params)
+    {
+        return $this->httpGet('/v2/c2c/repayment', $params, 'SIGN');
+    }
 }
