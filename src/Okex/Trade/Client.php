44b76109d79ff009e4ddcf7f027cefa82791ca36
wanlianbo <lianbo.wan@gmail.com>
2021-03-17 23:30:28 +0800

    add okex /api/v5/trade/order 下单 api

--- src/Okex/Trade/Client.php
+++ src/Okex/Trade/Client.php
@@ -6,4 +6,18 @@ use EasyExchange\Okex\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * 下单.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function order($params)
+    {
+        return $this->httpPostJson('/api/v5/trade/order', $params, [], 'SIGN');
+    }
 }
