6fb99de767e1f10385e9a0db7e2ef1d879a2d085
wanlianbo <lianbo.wan@gmail.com>
2021-03-18 23:12:31 +0800

    add okex /api/v5/trade/orders-algo-history 获取历史策略委托单列表 api

--- src/Okex/Algo/Client.php
+++ src/Okex/Algo/Client.php
@@ -50,4 +50,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/trade/orders-algo-pending', $params, 'SIGN');
     }
+
+    /**
+     * 获取历史策略委托单列表.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function history($params)
+    {
+        return $this->httpGet('/api/v5/trade/orders-algo-history', $params, 'SIGN');
+    }
 }
