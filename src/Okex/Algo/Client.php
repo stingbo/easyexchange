6cd4ae46b35749f08d37899ba3626090a6e9120f
wanlianbo <lianbo.wan@gmail.com>
2021-03-18 23:10:32 +0800

    add okex /api/v5/trade/cancel-algos 撤销策略委托订单 api

--- src/Okex/Algo/Client.php
+++ src/Okex/Algo/Client.php
@@ -20,4 +20,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/api/v5/trade/order-algo', $params, [], 'SIGN');
     }
+
+    /**
+     * 撤销策略委托订单.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function cancelOrder($params)
+    {
+        return $this->httpPostJson('/api/v5/trade/cancel-algos', $params, [], 'SIGN');
+    }
 }
