fc81af1ae28288c09495e044ed6488d6e32a8ba6
sting <sting_bo@163.com>
2021-03-18 20:14:17 +0800

    add okex /api/v5/trade/order 获取订单信息 api

--- src/Okex/Trade/Client.php
+++ src/Okex/Trade/Client.php
@@ -20,4 +20,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/api/v5/trade/order', $params, [], 'SIGN');
     }
+
+    /**
+     * 获取订单信息.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function get($params)
+    {
+        return $this->httpGet('/api/v5/trade/order', $params, 'SIGN');
+    }
 }
