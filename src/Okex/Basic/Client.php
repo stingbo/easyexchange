f4d06672de72c295d8eaa19b96a74c40a44e2d72
sting <sting_bo@163.com>
2021-03-19 09:10:54 +0800

    add okex /api/v5/public/mark-price 获取标记价格 api

--- src/Okex/Basic/Client.php
+++ src/Okex/Basic/Client.php
@@ -169,4 +169,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/public/liquidation-orders', $params);
     }
+
+    /**
+     * 获取标记价格.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function markPrice($params)
+    {
+        return $this->httpGet('/api/v5/public/mark-price', $params);
+    }
 }
