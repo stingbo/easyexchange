73314471b0feeda976ae8106a4e968a0e650404f
sting <sting_bo@163.com>
2021-03-19 09:09:52 +0800

    add okex /api/v5/public/liquidation-orders 获取平台公共爆仓单信息 api

--- src/Okex/Basic/Client.php
+++ src/Okex/Basic/Client.php
@@ -7,19 +7,6 @@ use EasyExchange\Okex\Kernel\BaseClient;
 class Client extends BaseClient
 {
     /**
-     * 获取当前系统时间戳.
-     *
-     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
-     *
-     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function systemTime()
-    {
-        return $this->httpGet('/api/v5/public/time');
-    }
-
-    /**
      * 获取所有可交易产品的信息列表.
      *
      * @param $params
@@ -153,5 +140,33 @@ class Client extends BaseClient
     public function discountRateInterestFreeQuota($ccy = '')
     {
         return $this->httpGet('/api/v5/public/discount-rate-interest-free-quota', compact('ccy'));
+    }
+
+    /**
+     * 获取当前系统时间戳.
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function systemTime()
+    {
+        return $this->httpGet('/api/v5/public/time');
+    }
+
+    /**
+     * 获取平台公共爆仓单信息.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function liquidationOrders($params)
+    {
+        return $this->httpGet('/api/v5/public/liquidation-orders', $params);
     }
 }
