53ed1f6767db97055d8937e278652856030f3486
sting <sting_bo@163.com>
2021-03-19 23:51:45 +0800

    add okex /api/v5/asset/deposit-history 充值记录 api

--- src/Okex/Wallet/Client.php
+++ src/Okex/Wallet/Client.php
@@ -65,4 +65,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/api/v5/asset/withdrawal', $params, [], 'SIGN');
     }
+
+    /**
+     * 充值记录.
+     *
+     * @param array $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function depositHistory($params = [])
+    {
+        return $this->httpGet('/api/v5/asset/deposit-history', $params, 'SIGN');
+    }
 }
