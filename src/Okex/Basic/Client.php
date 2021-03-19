c046edfb97f315108e9c6d73015283b6d34e40ca
sting <sting_bo@163.com>
2021-03-19 08:54:06 +0800

    add okex /api/v5/public/open-interest 获取持仓总量

--- src/Okex/Basic/Client.php
+++ src/Okex/Basic/Client.php
@@ -48,4 +48,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/public/delivery-exercise-history', $params);
     }
+
+    /**
+     * 获取持仓总量.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function openInterest($params)
+    {
+        return $this->httpGet('/api/v5/public/open-interest', $params);
+    }
 }
