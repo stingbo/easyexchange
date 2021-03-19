d8b3ce88362f84b58ca90cadff086ede934a5d9e
sting <sting_bo@163.com>
2021-03-20 00:15:03 +0800

    add okex /api/v5/market/history-candles 获取交易产品历史K线数据（仅主流币）

--- src/Okex/Market/Client.php
+++ src/Okex/Market/Client.php
@@ -83,4 +83,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/market/candles', $params);
     }
+
+    /**
+     * 获取交易产品历史K线数据（仅主流币）.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function klineHistory($params)
+    {
+        return $this->httpGet('/api/v5/market/history-candles', $params);
+    }
 }
