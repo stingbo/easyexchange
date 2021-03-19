fe6c2e27895b71a57216d8def0d9da707e45d73f
sting <sting_bo@163.com>
2021-03-19 21:53:37 +0800

    add okex /api/v5/account/positions 查看持仓信息 api

--- src/Okex/User/Client.php
+++ src/Okex/User/Client.php
@@ -20,4 +20,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/account/balance', compact('ccy'), 'SIGN');
     }
+
+    /**
+     * 查看持仓信息.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function positions($params)
+    {
+        return $this->httpGet('/api/v5/account/positions', $params, 'SIGN');
+    }
 }
