9f072663d30b720933cbad7e1f00f6b035ed8e16
sting <sting_bo@163.com>
2021-03-19 23:51:12 +0800

    add okex /api/v5/asset/withdrawal 提币 api

--- src/Okex/Wallet/Client.php
+++ src/Okex/Wallet/Client.php
@@ -50,4 +50,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/api/v5/asset/transfer', $params, [], 'SIGN');
     }
+
+    /**
+     * 提币.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function withdrawal($params)
+    {
+        return $this->httpPostJson('/api/v5/asset/withdrawal', $params, [], 'SIGN');
+    }
 }
