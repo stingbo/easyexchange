7994f02eef810588cec3f11b670c99f10891f7f1
sting <sting_bo@163.com>
2021-03-19 23:41:37 +0800

    add okex /api/v5/asset/balances 获取资金账户余额信息 api

--- src/Okex/Wallet/Client.php
+++ src/Okex/Wallet/Client.php
@@ -20,4 +20,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/asset/deposit-address', compact('ccy'), 'SIGN');
     }
+
+    /**
+     * 获取资金账户余额信息.
+     *
+     * @param string $ccy
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function balances($ccy = '')
+    {
+        return $this->httpGet('/api/v5/asset/balances', compact('ccy'), 'SIGN');
+    }
 }
