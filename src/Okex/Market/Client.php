3c59f8a3523160d25881cfa35ed88c6850006dbb
sting <sting_bo@163.com>
2021-03-20 00:07:33 +0800

    add okex /api/v5/market/tickers 获取所有产品行情信息

--- src/Okex/Market/Client.php
+++ src/Okex/Market/Client.php
@@ -7,6 +7,22 @@ use EasyExchange\Okex\Kernel\BaseClient;
 class Client extends BaseClient
 {
     /**
+     * 获取所有产品行情信息.
+     *
+     * @param $instType
+     * @param string $uly
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function tickers($instType, $uly = '')
+    {
+        return $this->httpGet('/api/v5/market/tickers', compact('instType', 'uly'));
+    }
+
+    /**
      * 获取产品深度.
      *
      * @param $instId
