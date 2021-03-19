62504a601274da0e6534379164d6c0cda3582a2d
sting <sting_bo@163.com>
2021-03-19 21:56:45 +0800

    add okex /api/v5/account/bills 账单流水查询（近七天） api

--- src/Okex/User/Client.php
+++ src/Okex/User/Client.php
@@ -35,4 +35,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/account/positions', $params, 'SIGN');
     }
+
+    /**
+     * 账单流水查询（近七天）.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function bills($params)
+    {
+        return $this->httpGet('/api/v5/account/bills', $params, 'SIGN');
+    }
 }
