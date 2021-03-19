642cb4170436e992c22d56848994132140ec5ad3
sting <sting_bo@163.com>
2021-03-19 23:41:22 +0800

    add okex /api/v5/asset/deposit-address 获取充值地址信息 api

--- src/Okex/Wallet/Client.php
+++ src/Okex/Wallet/Client.php
@@ -6,4 +6,18 @@ use EasyExchange\Okex\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * 获取充值地址信息.
+     *
+     * @param $ccy
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function depositAddress($ccy)
+    {
+        return $this->httpGet('/api/v5/asset/deposit-address', compact('ccy'), 'SIGN');
+    }
 }
