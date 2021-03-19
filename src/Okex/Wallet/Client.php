6f9fbfba77cc02cbf2bd6c2edd586842df27bc02
sting <sting_bo@163.com>
2021-03-19 23:50:39 +0800

    add okex /api/v5/asset/transfer 资金划转 api

--- src/Okex/Wallet/Client.php
+++ src/Okex/Wallet/Client.php
@@ -35,4 +35,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/asset/balances', compact('ccy'), 'SIGN');
     }
+
+    /**
+     * 资金划转.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function transfer($params)
+    {
+        return $this->httpPostJson('/api/v5/asset/transfer', $params, [], 'SIGN');
+    }
 }
