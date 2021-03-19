31cbcd15241a9fddae5475866dc4c2d5ef9c561c
sting <sting_bo@163.com>
2021-03-19 23:29:44 +0800

    add okex /api/v5/account/max-withdrawal 查看账户最大可转余额 api

--- src/Okex/User/Client.php
+++ src/Okex/User/Client.php
@@ -229,4 +229,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/api/v5/account/set-greeks', compact('greeksType'), [], 'SIGN');
     }
+
+    /**
+     * 查看账户最大可转余额.
+     *
+     * @param string $ccy
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function maxWithdrawal($ccy = '')
+    {
+        return $this->httpGet('/api/v5/account/max-withdrawal', compact('ccy'), 'SIGN');
+    }
 }
