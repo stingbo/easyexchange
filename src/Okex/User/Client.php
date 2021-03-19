b2f32ae520fecd4202052375025879ca05b5366a
sting <sting_bo@163.com>
2021-03-19 21:51:44 +0800

    add okex /api/v5/account/balance 查看账户余额 api

--- src/Okex/User/Client.php
+++ src/Okex/User/Client.php
@@ -6,4 +6,18 @@ use EasyExchange\Okex\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * 查看账户余额.
+     *
+     * @param string $ccy
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function balance($ccy = '')
+    {
+        return $this->httpGet('/api/v5/account/balance', compact('ccy'), 'SIGN');
+    }
 }
