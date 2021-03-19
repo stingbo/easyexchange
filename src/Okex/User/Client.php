52e3650c92e7f9bbe6ed5fd422081ac38400f444
sting <sting_bo@163.com>
2021-03-19 23:21:12 +0800

    add okex /api/v5/account/position/margin-balance 调整保证金 api

--- src/Okex/User/Client.php
+++ src/Okex/User/Client.php
@@ -138,4 +138,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/account/max-avail-size', $params, 'SIGN');
     }
+
+    /**
+     * 调整保证金.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function marginBalance($params)
+    {
+        return $this->httpPostJson('/api/v5/account/position/margin-balance', $params, [], 'SIGN');
+    }
 }
