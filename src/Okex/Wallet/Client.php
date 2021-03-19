754b0332df2e2bc224cb38e01dc8f4d4b7415bfc
sting <sting_bo@163.com>
2021-03-19 23:53:49 +0800

    add okex /api/v5/asset/bills 资金流水查询

--- src/Okex/Wallet/Client.php
+++ src/Okex/Wallet/Client.php
@@ -123,4 +123,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/api/v5/asset/purchase_redempt', $params, [], 'SIGN');
     }
+
+    /**
+     * 资金流水查询.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function bills($params)
+    {
+        return $this->httpGet('/api/v5/asset/bills', $params, 'SIGN');
+    }
 }
