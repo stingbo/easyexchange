d3ae9af0cdfb0f137f8b0b1df3fe8b2ab4e7ec25
sting <sting_bo@163.com>
2021-03-19 23:19:55 +0800

    add okex /api/v5/account/max-avail-size 获取最大可用数量 api

--- src/Okex/User/Client.php
+++ src/Okex/User/Client.php
@@ -123,4 +123,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/account/max-size', $params, 'SIGN');
     }
+
+    /**
+     * 获取最大可用数量.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function maxAvailSize($params)
+    {
+        return $this->httpGet('/api/v5/account/max-avail-size', $params, 'SIGN');
+    }
 }
