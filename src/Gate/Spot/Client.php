a046940889294babf44a3b157ae9cd8cf646323d
sting <sting_bo@163.com>
2021-03-21 20:59:41 +0800

    add gate /api/v4/spot/currency_pairs List all currency pairs supported.

--- src/Gate/Spot/Client.php
+++ src/Gate/Spot/Client.php
@@ -33,4 +33,17 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/api/v4/spot/currencies/%s', $currency));
     }
+
+    /**
+     * List all currency pairs supported.
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function currencyPairs()
+    {
+        return $this->httpGet('/api/v4/spot/currency_pairs');
+    }
 }
