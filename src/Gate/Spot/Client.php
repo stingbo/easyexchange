e2c9e63ccad06a41922af8230e919e26d3d3e0a5
sting <sting_bo@163.com>
2021-03-21 21:02:22 +0800

    add gate /api/v4/spot/currency_pairs/%s Get detail of one single order.

--- src/Gate/Spot/Client.php
+++ src/Gate/Spot/Client.php
@@ -46,4 +46,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v4/spot/currency_pairs');
     }
+
+    /**
+     * Get detail of one single order.
+     *
+     * @param $currency_pair
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function currencyPair($currency_pair)
+    {
+        return $this->httpGet(sprintf('/api/v4/spot/currency_pairs/%s', $currency_pair));
+    }
 }
