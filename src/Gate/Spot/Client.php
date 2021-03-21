74e28704bb0dfbbcf0315e79a37b4d03f12618bb
sting <sting_bo@163.com>
2021-03-21 21:23:42 +0800

    add gate /api/v4/spot/candlesticks Market candlesticks.

--- src/Gate/Spot/Client.php
+++ src/Gate/Spot/Client.php
@@ -106,4 +106,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v4/spot/trades', $params);
     }
+
+    /**
+     * Market candlesticks.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function kline($params)
+    {
+        return $this->httpGet('/api/v4/spot/candlesticks', $params);
+    }
 }
