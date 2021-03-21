2dc315d0f5ee009908c0ac702fad32948283a905
sting <sting_bo@163.com>
2021-03-21 20:53:03 +0800

    add gate /api/v4/spot/currencies List all currencies' detail.

--- src/Gate/Spot/Client.php
+++ src/Gate/Spot/Client.php
@@ -6,4 +6,16 @@ use EasyExchange\Gate\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * List all currencies' detail.
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function currencies()
+    {
+        return $this->httpGet('/api/v4/spot/currencies');
+    }
 }
