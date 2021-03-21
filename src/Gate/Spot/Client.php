dbff3dc65f2c073573eef068862532f9e73e108c
sting <sting_bo@163.com>
2021-03-21 20:56:31 +0800

    add gate /api/v4/spot/currencies/%s Get detail of one particular currency.

--- src/Gate/Spot/Client.php
+++ src/Gate/Spot/Client.php
@@ -18,4 +18,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v4/spot/currencies');
     }
+
+    /**
+     * Get detail of one particular currency.
+     *
+     * @param $currency
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function currency($currency)
+    {
+        return $this->httpGet(sprintf('/api/v4/spot/currencies/%s', $currency));
+    }
 }
