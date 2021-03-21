f8b34e415f361c12751bc80dcaa1fbafa526f03c
sting <sting_bo@163.com>
2021-03-21 20:35:33 +0800

    add gate spot api

---
+++ src/Gate/Spot/Client.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EasyExchange\Gate\Spot;
+
+use EasyExchange\Gate\Kernel\BaseClient;
+
+class Client extends BaseClient
+{
+}
---
+++ src/Gate/Spot/ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace EasyExchange\Gate\Spot;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+/**
+ * Class ServiceProvider.
+ */
+class ServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $app)
+    {
+        $app['spot'] = function ($app) {
+            return new Client($app);
+        };
+    }
+}
