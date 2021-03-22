8a6fa02d639927ebba7fc4a0e86abdf2fc58815f
sting <sting_bo@163.com>
2021-03-22 09:03:57 +0800

    add /api/v4/wallet/transfers Transfer between trading accounts.

--- src/Gate/Wallet/Client.php
+++ src/Gate/Wallet/Client.php
@@ -50,4 +50,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v4/wallet/deposits', $params, 'SIGN');
     }
+
+    /**
+     * Transfer between trading accounts.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function transfer($params)
+    {
+        return $this->httpPostJson('/api/v4/wallet/transfers', $params, [], 'SIGN');
+    }
 }
