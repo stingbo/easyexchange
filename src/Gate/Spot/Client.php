ba2a5be6ccccbbd4e367a284a1b07c10cba2e5a8
wanlianbo <lianbo.wan@gmail.com>
2021-03-22 22:55:18 +0800

    add gate /api/v4/spot/orders Cancel all open orders in specified currency pair.

--- src/Gate/Spot/Client.php
+++ src/Gate/Spot/Client.php
@@ -197,4 +197,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v4/spot/orders', $params, 'SIGN');
     }
+
+    /**
+     * Cancel all open orders in specified currency pair.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function cancelOrder($params)
+    {
+        return $this->httpDelete('/api/v4/spot/orders', $params, 'SIGN');
+    }
 }
