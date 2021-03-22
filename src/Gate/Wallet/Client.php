4d565f1dd486e3fe92d13c0a9713146872ca4f93
wanlianbo <lianbo.wan@gmail.com>
2021-03-22 21:58:06 +0800

    add /api/v4/wallet/sub_account_transfers Transfer records between main and sub accounts.

--- src/Gate/Wallet/Client.php
+++ src/Gate/Wallet/Client.php
@@ -80,4 +80,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/api/v4/wallet/sub_account_transfers', $params, [], 'SIGN');
     }
+
+    /**
+     * Transfer records between main and sub accounts.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function subAccountTransferHistory($params)
+    {
+        return $this->httpGet('/api/v4/wallet/sub_account_transfers', $params, 'SIGN');
+    }
 }
