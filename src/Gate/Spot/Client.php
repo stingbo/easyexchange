b78093182a6d4cc0766c52d0c4bd48f545ede623
wanlianbo <lianbo.wan@gmail.com>
2021-03-22 22:31:30 +0800

    add /api/v4/spot/open_orders List all open orders.

--- src/Gate/Spot/Client.php
+++ src/Gate/Spot/Client.php
@@ -166,4 +166,20 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/api/v4/spot/batch_orders', $params, [], 'SIGN');
     }
+
+    /**
+     * List all open orders.
+     *
+     * @param string $page
+     * @param string $limit
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function openOrders($page = '', $limit = '')
+    {
+        return $this->httpGet('/api/v4/spot/open_orders', compact('page', 'limit'), 'SIGN');
+    }
 }
