fcf4a59cf838f6fad1d91153722169fcbf097d6e
sting <sting_bo@163.com>
2021-03-22 08:59:25 +0800

    add gate /api/v4/wallet/withdrawals Retrieve withdrawal records.

--- src/Gate/Wallet/Client.php
+++ src/Gate/Wallet/Client.php
@@ -20,4 +20,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v4/wallet/deposit_address', compact('currency'), 'SIGN');
     }
+
+    /**
+     * Retrieve withdrawal records.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function withdrawHistory($params)
+    {
+        return $this->httpGet('/api/v4/wallet/withdrawals', $params, 'SIGN');
+    }
 }
