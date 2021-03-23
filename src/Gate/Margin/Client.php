6221334c7c7f78eb8522e6eaf1f46c183ddcdde7
sting <sting_bo@163.com>
2021-03-23 09:23:17 +0800

    add gate /api/v4/margin/merged_loans Merge multiple lending loans.

--- src/Gate/Margin/Client.php
+++ src/Gate/Margin/Client.php
@@ -123,4 +123,20 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v4/margin/loans', $params, 'SIGN');
     }
+
+    /**
+     * Merge multiple lending loans.
+     *
+     * @param $currency
+     * @param $ids
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function mergeLoan($currency, $ids)
+    {
+        return $this->httpPostJson('/api/v4/margin/merged_loans', compact('currency', 'ids'), [], 'SIGN');
+    }
 }
