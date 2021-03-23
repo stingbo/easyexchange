78f3862540ff6f5b30c6cc01b8aa756cf976c92d
sting <sting_bo@163.com>
2021-03-23 09:10:35 +0800

    add gate /api/v4/margin/funding_book Order book of lending loans.

--- src/Gate/Margin/Client.php
+++ src/Gate/Margin/Client.php
@@ -33,4 +33,19 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/api/v4/margin/currency_pairs/%s', $currency_pair));
     }
+
+    /**
+     * Order book of lending loans.
+     *
+     * @param $currency
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function depth($currency)
+    {
+        return $this->httpGet('/api/v4/margin/funding_book', compact('currency'));
+    }
 }
