c165aa69e0aa550023246b2dc7ba0e65f616fdb1
sting <sting_bo@163.com>
2021-03-23 09:17:48 +0800

    add gate /api/v4/margin/funding_accounts Funding account list.

--- src/Gate/Margin/Client.php
+++ src/Gate/Margin/Client.php
@@ -78,4 +78,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v4/margin/account_book', $params, 'SIGN');
     }
+
+    /**
+     * Funding account list.
+     *
+     * @param string $currency
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function fundingAccounts($currency = '')
+    {
+        return $this->httpGet('/api/v4/margin/funding_accounts', compact('currency'), 'SIGN');
+    }
 }
