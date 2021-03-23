6c23e8c2c3a2865e1289c605ae735990085f9cf3
sting <sting_bo@163.com>
2021-03-23 09:21:32 +0800

    add gate /api/v4/margin/loans List all loans.

--- src/Gate/Margin/Client.php
+++ src/Gate/Margin/Client.php
@@ -108,4 +108,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/api/v4/margin/loans', $params, [], 'SIGN');
     }
+
+    /**
+     * List all loans.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function loanHistory($params)
+    {
+        return $this->httpGet('/api/v4/margin/loans', $params, 'SIGN');
+    }
 }
