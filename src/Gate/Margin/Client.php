ef72f703ee3d8282b22477966a35494354e06e5d
sting <sting_bo@163.com>
2021-03-23 09:26:29 +0800

    add gate /api/v4/margin/loans/%s Retrieve one single loan detail.

--- src/Gate/Margin/Client.php
+++ src/Gate/Margin/Client.php
@@ -139,4 +139,20 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/api/v4/margin/merged_loans', compact('currency', 'ids'), [], 'SIGN');
     }
+
+    /**
+     * Retrieve one single loan detail.
+     *
+     * @param $loan_id
+     * @param $side
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getLoan($loan_id, $side)
+    {
+        return $this->httpGet(sprintf('/api/v4/margin/loans/%s', $loan_id), compact('side'), 'SIGN');
+    }
 }
