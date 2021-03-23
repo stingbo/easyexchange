1c08a5068e166316be854941cf1e82dcd5ee1e1d
sting <sting_bo@163.com>
2021-03-23 09:03:04 +0800

    add gate /api/v4/margin/currency_pairs List all supported currency pairs supported in margin trading.

--- src/Gate/Margin/Client.php
+++ src/Gate/Margin/Client.php
@@ -6,4 +6,16 @@ use EasyExchange\Gate\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * List all supported currency pairs supported in margin trading.
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function currencyPairs()
+    {
+        return $this->httpGet('/api/v4/margin/currency_pairs');
+    }
 }
