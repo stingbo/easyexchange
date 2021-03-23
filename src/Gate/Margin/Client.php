30438274ecf0be8c3f4f8c03f0df0c7819316249
sting <sting_bo@163.com>
2021-03-23 09:04:43 +0800

    add gate /api/v4/margin/currency_pairs/%s Query one single margin currency pair.

--- src/Gate/Margin/Client.php
+++ src/Gate/Margin/Client.php
@@ -18,4 +18,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v4/margin/currency_pairs');
     }
+
+    /**
+     * Query one single margin currency pair.
+     *
+     * @param $currency_pair
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function currencyPair($currency_pair)
+    {
+        return $this->httpGet(sprintf('/api/v4/margin/currency_pairs/%s', $currency_pair));
+    }
 }
