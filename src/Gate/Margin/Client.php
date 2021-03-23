8b47ed676c14d40cb3285870fda449186f0701cf
sting <sting_bo@163.com>
2021-03-23 09:16:01 +0800

    add gate /api/v4/margin/account_book List margin account balance change history.

--- src/Gate/Margin/Client.php
+++ src/Gate/Margin/Client.php
@@ -63,4 +63,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v4/margin/accounts', compact('currency_pair'), 'SIGN');
     }
+
+    /**
+     * List margin account balance change history.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function accountHistory($params)
+    {
+        return $this->httpGet('/api/v4/margin/account_book', $params, 'SIGN');
+    }
 }
