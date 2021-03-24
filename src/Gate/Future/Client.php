b5e94463510f3f4e93202a486586926c6ad4bdcf
wanlianbo <lianbo.wan@gmail.com>
2021-03-24 22:02:29 +0800

    add gate /api/v4/futures/%s/trades Futures trading history.

--- src/Gate/Future/Client.php
+++ src/Gate/Future/Client.php
@@ -52,4 +52,20 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/api/v4/futures/%s/order_book', $settle), $params);
     }
+
+    /**
+     * Futures trading history.
+     *
+     * @param $settle
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function trades($settle, $params)
+    {
+        return $this->httpGet(sprintf('/api/v4/futures/%s/trades', $settle), $params);
+    }
 }
