3bfd238bd6d6681ef79f85785c14a63290050b64
wanlianbo <lianbo.wan@gmail.com>
2021-03-24 22:14:24 +0800

    add gate /api/v4/futures/%s/funding_rate Funding rate history.

--- src/Gate/Future/Client.php
+++ src/Gate/Future/Client.php
@@ -100,4 +100,20 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/api/v4/futures/%s/tickers', $settle), compact('contract'));
     }
+
+    /**
+     * Funding rate history.
+     *
+     * @param $settle
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function fundingRateHistory($settle, $params)
+    {
+        return $this->httpGet(sprintf('/api/v4/futures/%s/funding_rate', $settle), $params);
+    }
 }
