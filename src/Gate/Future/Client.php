aac8000d9e832c61da07f6e936db22a44af8555f
wanlianbo <lianbo.wan@gmail.com>
2021-03-25 21:53:18 +0800

    add gate /api/v4/futures/%s/orders List futures orders.

--- src/Gate/Future/Client.php
+++ src/Gate/Future/Client.php
@@ -376,4 +376,20 @@ class Client extends BaseClient
     {
         return $this->httpPostJson(sprintf('/api/v4/futures/%s/orders', $settle), $params, [], 'SIGN');
     }
+
+    /**
+     * List futures orders.
+     *
+     * @param $settle
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function orders($settle, $params)
+    {
+        return $this->httpGet(sprintf('/api/v4/futures/%s/orders', $settle), $params, 'SIGN');
+    }
 }
