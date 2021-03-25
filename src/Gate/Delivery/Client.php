97e618b63478ec48022616eed9435762157171a3
wanlianbo <lianbo.wan@gmail.com>
2021-03-25 22:45:56 +0800

    add gate /api/v4/delivery/%s/contracts List all futures contracts

--- src/Gate/Delivery/Client.php
+++ src/Gate/Delivery/Client.php
@@ -6,4 +6,18 @@ use EasyExchange\Gate\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * List all futures contracts.
+     *
+     * @param $settle
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function contracts($settle)
+    {
+        return $this->httpGet(sprintf('/api/v4/delivery/%s/contracts', $settle));
+    }
 }
