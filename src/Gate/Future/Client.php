c84f4b64c5798cc615c4b62026efc5541eccee43
sting <sting_bo@163.com>
2021-03-25 09:00:28 +0800

    add gate /api/v4/futures/%s/positions/%s/leverage Update position leverage.

--- src/Gate/Future/Client.php
+++ src/Gate/Future/Client.php
@@ -243,4 +243,21 @@ class Client extends BaseClient
     {
         return $this->httpPostJson(sprintf('/api/v4/futures/%s/positions/%s/margin', $settle, $contract), compact('change'), [], 'SIGN');
     }
+
+    /**
+     * Update position leverage.
+     *
+     * @param $settle
+     * @param $contract
+     * @param $change
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function modifyPositionLeverage($settle, $contract, $change)
+    {
+        return $this->httpPostJson(sprintf('/api/v4/futures/%s/positions/%s/leverage', $settle, $contract), compact('change'), [], 'SIGN');
+    }
 }
