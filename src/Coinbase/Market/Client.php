368bc3e9233d62f8ba58975221aa835fc1668656
sting <sting_bo@163.com>
2021-03-26 23:13:26 +0800

    add coinbase market Get Historic Rates - Historic rates for a product. Rates are returned in grouped buckets based on requested granularity.

--- src/Coinbase/Market/Client.php
+++ src/Coinbase/Market/Client.php
@@ -79,4 +79,19 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/products/%s/trades', $product_id));
     }
+
+    /**
+     * Get Historic Rates - Historic rates for a product. Rates are returned in grouped buckets based on requested granularity.
+     *
+     * @param $product_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function kline($product_id)
+    {
+        return $this->httpGet(sprintf('/products/%s/candles', $product_id));
+    }
 }
