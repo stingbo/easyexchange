3dffa896556177bfebc08ee7e7735a6afaccd852
sting <sting_bo@163.com>
2021-03-26 23:11:35 +0800

    add coinbase Get Single market Product - Get market data for a specific currency pair.

--- src/Coinbase/Market/Client.php
+++ src/Coinbase/Market/Client.php
@@ -7,8 +7,7 @@ use EasyExchange\Gate\Kernel\BaseClient;
 class Client extends BaseClient
 {
     /**
-     * Get Products.
-     * Get a list of available currency pairs for trading.
+     * Get Products - Get a list of available currency pairs for trading.
      *
      * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
      *
@@ -18,5 +17,20 @@ class Client extends BaseClient
     public function products()
     {
         return $this->httpGet('/products');
+    }
+
+    /**
+     * Get Single Product - Get market data for a specific currency pair.
+     *
+     * @param $product_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function product($product_id)
+    {
+        return $this->httpGet(sprintf('/products/%s', $product_id));
     }
 }
