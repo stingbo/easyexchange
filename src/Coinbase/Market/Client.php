36381796be6761bd84a24122a89729c90ba1444f
sting <sting_bo@163.com>
2021-03-26 23:12:42 +0800

    add coinbase market Get Product Ticker - Snapshot information about the last trade (tick), best bid/ask and 24h volume.

--- src/Coinbase/Market/Client.php
+++ src/Coinbase/Market/Client.php
@@ -49,4 +49,19 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/products/%s/book', $product_id), compact('level'));
     }
+
+    /**
+     * Get Product Ticker - Snapshot information about the last trade (tick), best bid/ask and 24h volume.
+     *
+     * @param $product_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function tickers($product_id)
+    {
+        return $this->httpGet(sprintf('/products/%s/ticker', $product_id));
+    }
 }
