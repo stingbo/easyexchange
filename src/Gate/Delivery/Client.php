86c79d5f8247c1e1e8527324dfc8dad315214fbf
sting <sting_bo@163.com>
2021-03-26 09:02:53 +0800

    add gate delivery List liquidation history.

--- src/Gate/Delivery/Client.php
+++ src/Gate/Delivery/Client.php
@@ -341,4 +341,20 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/api/v4/delivery/%s/position_close', $settle), $params, 'SIGN');
     }
+
+    /**
+     * List liquidation history.
+     *
+     * @param $settle
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function forceLiquidationRec($settle, $params)
+    {
+        return $this->httpGet(sprintf('/api/v4/delivery/%s/liquidates', $settle), $params, 'SIGN');
+    }
 }
