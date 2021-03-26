35ca2f776e2b1314c1c864a7be8ca6c91d510752
sting <sting_bo@163.com>
2021-03-26 08:40:43 +0800

    add gate delivery Futures order book.

--- src/Gate/Delivery/Client.php
+++ src/Gate/Delivery/Client.php
@@ -36,4 +36,20 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/api/v4/delivery/%s/contracts/%s', $settle, $contract));
     }
+
+    /**
+     * Futures order book.
+     *
+     * @param $settle
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function depth($settle, $params)
+    {
+        return $this->httpGet(sprintf('/api/v4/delivery/%s/order_book', $settle), $params);
+    }
 }
