8b58ae265d123da1caac4ee614797685b183d49f
sting <sting_bo@163.com>
2021-03-26 08:56:34 +0800

    add gate delivery Futures insurance balance history.

--- src/Gate/Delivery/Client.php
+++ src/Gate/Delivery/Client.php
@@ -100,4 +100,20 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/api/v4/delivery/%s/tickers', $settle), compact('contract'));
     }
+
+    /**
+     * Futures insurance balance history.
+     *
+     * @param $settle
+     * @param string $limit
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function insuranceHistory($settle, $limit = '')
+    {
+        return $this->httpGet(sprintf('/api/v4/delivery/%s/insurance', $settle), compact('limit'));
+    }
 }
