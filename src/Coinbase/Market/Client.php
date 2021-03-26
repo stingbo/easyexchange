aacbee2a0eca9d310e78a17ad284108ade618fd6
sting <sting_bo@163.com>
2021-03-26 23:14:05 +0800

    add coinbase market Get currencies - List known currencies.

--- src/Coinbase/Market/Client.php
+++ src/Coinbase/Market/Client.php
@@ -109,4 +109,17 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/products/%s/stats', $product_id));
     }
+
+    /**
+     * Get currencies - List known currencies.
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function currencies()
+    {
+        return $this->httpGet('/currencies');
+    }
 }
