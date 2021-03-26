1c1dc896830e912a2d99149cff43b6d2695e7a7e
sting <sting_bo@163.com>
2021-03-26 08:55:22 +0800

    add gate delivery Get futures candlesticks.

--- src/Gate/Delivery/Client.php
+++ src/Gate/Delivery/Client.php
@@ -68,4 +68,20 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/api/v4/delivery/%s/trades', $settle), $params);
     }
+
+    /**
+     * Get futures candlesticks.
+     *
+     * @param $settle
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function kline($settle, $params)
+    {
+        return $this->httpGet(sprintf('/api/v4/delivery/%s/candlesticks', $settle), $params);
+    }
 }
