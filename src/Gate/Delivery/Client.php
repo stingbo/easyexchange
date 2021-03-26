d8a5360a7448954c09de4ddd007baad542643468
sting <sting_bo@163.com>
2021-03-26 08:58:38 +0800

    add gate delivery Get single position.

--- src/Gate/Delivery/Client.php
+++ src/Gate/Delivery/Client.php
@@ -162,4 +162,20 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/api/v4/delivery/%s/positions', $settle), [], 'SIGN');
     }
+
+    /**
+     * Get single position.
+     *
+     * @param $settle
+     * @param $contract
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function position($settle, $contract)
+    {
+        return $this->httpGet(sprintf('/api/v4/delivery/%s/positions/%s', $settle, $contract), [], 'SIGN');
+    }
 }
