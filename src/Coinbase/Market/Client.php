f978af9cba5192cbeba3bc78998a1c5769474db6
sting <sting_bo@163.com>
2021-03-26 23:13:03 +0800

    add coinbase market Get Trades - List the latest trades for a product.

--- src/Coinbase/Market/Client.php
+++ src/Coinbase/Market/Client.php
@@ -64,4 +64,19 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/products/%s/ticker', $product_id));
     }
+
+    /**
+     * Get Trades - List the latest trades for a product.
+     *
+     * @param $product_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function trades($product_id)
+    {
+        return $this->httpGet(sprintf('/products/%s/trades', $product_id));
+    }
 }
