7d622a062125111bf7160d4147d794697e82368e
sting <sting_bo@163.com>
2021-03-26 09:03:31 +0800

    add gate delivery List settlement history.

--- src/Gate/Delivery/Client.php
+++ src/Gate/Delivery/Client.php
@@ -357,4 +357,20 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/api/v4/delivery/%s/liquidates', $settle), $params, 'SIGN');
     }
+
+    /**
+     * List settlement history.
+     *
+     * @param $settle
+     * @param array $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function settlements($settle, $params = [])
+    {
+        return $this->httpGet(sprintf('/api/v4/delivery/%s/settlements', $settle), $params, 'SIGN');
+    }
 }
