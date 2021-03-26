aa76dfc0a855e0b91f809ac93beb60b30a01977f
sting <sting_bo@163.com>
2021-03-26 09:01:36 +0800

    add gate delivery List personal trading history.

--- src/Gate/Delivery/Client.php
+++ src/Gate/Delivery/Client.php
@@ -309,4 +309,20 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/api/v4/delivery/%s/orders/%s', $settle, $order_id), [], 'SIGN');
     }
+
+    /**
+     * List personal trading history.
+     *
+     * @param $settle
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function myTrades($settle, $params)
+    {
+        return $this->httpGet(sprintf('/api/v4/delivery/%s/my_trades', $settle), $params, 'SIGN');
+    }
 }
