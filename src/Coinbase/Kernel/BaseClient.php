4ce5113beffb8e52757c475f9ffecea47ac8df8c
sting <sting_bo@163.com>
2021-03-27 12:43:29 +0800

    update Authentication

--- src/Coinbase/Kernel/BaseClient.php
+++ src/Coinbase/Kernel/BaseClient.php
@@ -8,6 +8,8 @@ class BaseClient extends \EasyExchange\Kernel\BaseClient
 {
     public $timestamp;
 
+    public $message;
+
     /**
      * 获取签名.
      *
@@ -15,13 +17,18 @@ class BaseClient extends \EasyExchange\Kernel\BaseClient
      *
      * @return string
      */
-    public function getSignature($query, $body, $method, $uri_path)
+    public function getSignature($timestamp, $method = '', $uri_path = '', $params = [])
     {
-        $data = hash('sha512', (null !== $body) ? $body : '');
-        $sign_param = $method."\n".$uri_path."\n".$query."\n".$data."\n".$this->timestamp;
+        if ('GET' == $method) {
+            $uri_path .= $params ? '?'.http_build_query($params) : '';
+            $params = [];
+        }
+        $body = $params ? json_encode($params, JSON_UNESCAPED_SLASHES) : '';
+
+        $this->message = (string) $timestamp.$method.$uri_path.(string) $body;
         $secret = $this->app->config->get('secret');
 
-        return hash_hmac('sha512', $sign_param, $secret);
+        return base64_encode(hash_hmac('sha256', $this->message, $secret, true));
     }
 
     /**
@@ -32,12 +39,16 @@ class BaseClient extends \EasyExchange\Kernel\BaseClient
         $this->pushMiddleware($this->addHeaderMiddleware('Content-Type', 'application/json'), 'add_header_content_type');
         if ('SIGN' == $this->sign_type) {
             // signature
+            $this->timestamp = $this->getRequestDateTime();
+            $this->pushMiddleware($this->addHeaderMiddleware('CB-ACCESS-KEY', $this->app->config->get('app_key')), 'add_header_appkey');
+            $this->pushMiddleware($this->addHeaderMiddleware('CB-ACCESS-TIMESTAMP', $this->timestamp), 'add_header_timestamp');
+            $this->pushMiddleware($this->addHeaderMiddleware('CB-ACCESS-PASSPHRASE', $this->app->config->get('passphrase')), 'add_header_passphrase');
             $this->pushMiddleware($this->signatureMiddleware(), 'signature');
         }
     }
 
     /**
-     * Attache signature to request query.
+     * Attache signature to request header.
      *
      * @return \Closure
      */
@@ -45,20 +56,30 @@ class BaseClient extends \EasyExchange\Kernel\BaseClient
     {
         return function (callable $handler) {
             return function (RequestInterface $request, array $options) use ($handler) {
-                $key = $this->app->config->get('app_key');
-                $this->timestamp = time();
                 $method = $request->getMethod();
                 $uri_path = $request->getUri()->getPath();
-                $query = $request->getUri()->getQuery();
-                $body = $request->getBody();
-                $signature = $this->getSignature($query, $body, $method, $uri_path);
-
-                $request = $request->withHeader('KEY', $key);
-                $request = $request->withHeader('Timestamp', $this->timestamp);
-                $request = $request->withHeader('SIGN', $signature);
+                if ('POST' == $method) { // POST
+                    $body = $request->getBody();
+                    $params = json_decode($body, true);
+                    $signature = $this->getSignature($this->timestamp, $method, $uri_path, $params);
+                } else { // GET
+                    parse_str($request->getUri()->getQuery(), $query);
+                    $signature = $this->getSignature($this->timestamp, $method, $uri_path, $query);
+                }
+                $request = $request->withHeader('CB-ACCESS-SIGN', $signature);
 
                 return $handler($request, $options);
             };
         };
+    }
+
+    /**
+     * A timestamp for your request.
+     *
+     * @return float
+     */
+    public function getRequestDateTime()
+    {
+        return time();
     }
 }
