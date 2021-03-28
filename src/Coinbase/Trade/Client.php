52044c3cb899a58ce74c59a79968f159cacc9f82
sting <sting_bo@163.com>
2021-03-28 22:42:29 +0800

    add coinbase Place a New Order.

--- src/Coinbase/Trade/Client.php
+++ src/Coinbase/Trade/Client.php
@@ -6,4 +6,18 @@ use EasyExchange\Coinbase\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * Place a New Order.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function order($params)
+    {
+        return $this->httpPostJson('/orders', $params, [], 'SIGN');
+    }
 }
