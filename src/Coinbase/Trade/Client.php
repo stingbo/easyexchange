a601cd7c4cbd71ed89972cc18a72fff797956ea7
sting <sting_bo@163.com>
2021-03-28 23:25:13 +0800

    add coinbase Cancel an Order.

--- src/Coinbase/Trade/Client.php
+++ src/Coinbase/Trade/Client.php
@@ -3,6 +3,7 @@
 namespace EasyExchange\Coinbase\Trade;
 
 use EasyExchange\Coinbase\Kernel\BaseClient;
+use EasyExchange\Kernel\Exceptions\InvalidArgumentException;
 
 class Client extends BaseClient
 {
@@ -19,5 +20,32 @@ class Client extends BaseClient
     public function order($params)
     {
         return $this->httpPostJson('/orders', $params, [], 'SIGN');
+    }
+
+    /**
+     * Cancel an Order.
+     *
+     * @param string $id
+     * @param string $client_oid
+     * @param string $product_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws InvalidArgumentException
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function cancelOrder($id = '', $client_oid = '', $product_id = '')
+    {
+        if (!$id && !$client_oid) {
+            throw new InvalidArgumentException('Order id or client_id cannot be empty');
+        }
+        if ($id) {
+            $url = sprintf('/orders/%s', $id);
+        } elseif ($client_oid) {
+            $url = sprintf('/orders/%s', $client_oid);
+        }
+
+        return $this->httpDelete($url, compact('product_id'), 'SIGN');
     }
 }
