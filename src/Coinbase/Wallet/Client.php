89be9bfe03f3ce3b191e193e146108a5cff874f3
wanlianbo <lianbo.wan@gmail.com>
2021-03-29 22:21:21 +0800

    coinbase wallet Get Current Exchange Limits.

--- src/Coinbase/Wallet/Client.php
+++ src/Coinbase/Wallet/Client.php
@@ -6,4 +6,16 @@ use EasyExchange\Coinbase\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * Get Current Exchange Limits.
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function exchangeLimits()
+    {
+        return $this->httpGet('/users/self/exchange-limits', [], 'SIGN');
+    }
 }
