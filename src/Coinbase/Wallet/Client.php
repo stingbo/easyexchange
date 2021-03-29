0ff95477fb4237e7424832aea7d146ae499f7c2f
wanlianbo <lianbo.wan@gmail.com>
2021-03-29 22:35:09 +0800

    coinbase Single Deposit Or Single Withdrawal.

--- src/Coinbase/Wallet/Client.php
+++ src/Coinbase/Wallet/Client.php
@@ -33,4 +33,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/transfers', $params, 'SIGN');
     }
+
+    /**
+     * Single Deposit Or Single Withdrawal.
+     *
+     * @param $transfer_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getTransfer($transfer_id)
+    {
+        return $this->httpGet(sprintf('/transfers/%s', $transfer_id), [], 'SIGN');
+    }
 }
