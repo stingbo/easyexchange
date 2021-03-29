d900fee49e8307ae39b61b1fe382784d1506e990
sting <sting_bo@163.com>
2021-03-29 09:12:14 +0800

    add coinbase List Orders.

--- src/Coinbase/Trade/Client.php
+++ src/Coinbase/Trade/Client.php
@@ -63,4 +63,19 @@ class Client extends BaseClient
     {
         return $this->httpDelete('/orders', compact('product_id'), 'SIGN');
     }
+
+    /**
+     * List Orders.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function orders($params)
+    {
+        return $this->httpGet('/orders', $params, 'SIGN');
+    }
 }
