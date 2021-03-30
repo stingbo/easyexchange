ef38c138a7d49fa7057b37642dd0c176e28b95a0
sting <sting_bo@163.com>
2021-03-30 08:52:51 +0800

    coinbase wallet Payment method - Deposit funds from a payment method.

--- src/Coinbase/Wallet/Client.php
+++ src/Coinbase/Wallet/Client.php
@@ -61,4 +61,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/payment-methods', [], 'SIGN');
     }
+
+    /**
+     * Payment method - Deposit funds from a payment method.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function deposit($params)
+    {
+        return $this->httpPostJson('/deposits/payment-method', $params, [], 'SIGN');
+    }
 }
