163cb3da5ebb67c18d31f3a1c527975b8a4e5887
sting <sting_bo@163.com>
2021-03-31 09:19:00 +0800

    coinbase user Get a Profile.

--- src/Coinbase/User/Client.php
+++ src/Coinbase/User/Client.php
@@ -104,4 +104,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/profiles', [], 'SIGN');
     }
+
+    /**
+     * Get a Profile.
+     *
+     * @param $profile_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function profile($profile_id)
+    {
+        return $this->httpGet(sprintf('/profiles/%s', $profile_id), [], 'SIGN');
+    }
 }
