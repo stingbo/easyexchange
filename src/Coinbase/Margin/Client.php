4f1a8c237bcb201c73552ccdca5ddbf89a395a92
wanlianbo <lianbo.wan@gmail.com>
2021-03-31 22:14:55 +0800

    coinbase margin Get buying power or selling power.

--- src/Coinbase/Margin/Client.php
+++ src/Coinbase/Margin/Client.php
@@ -20,4 +20,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/margin/profile_information', compact('product_id'), 'SIGN');
     }
+
+    /**
+     * Get buying power or selling power.
+     *
+     * @param $product_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function power($product_id)
+    {
+        return $this->httpGet('/margin/buying_power', compact('product_id'), 'SIGN');
+    }
 }
