370cbdfd4175b62a68118fc2e5620e79d4335254
wanlianbo <lianbo.wan@gmail.com>
2021-03-31 22:13:28 +0800

    coinbase margin Get margin profile information.

--- src/Coinbase/Margin/Client.php
+++ src/Coinbase/Margin/Client.php
@@ -6,4 +6,18 @@ use EasyExchange\Coinbase\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * Get margin profile information.
+     *
+     * @param $product_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function profileInformation($product_id)
+    {
+        return $this->httpGet('/margin/profile_information', compact('product_id'), 'SIGN');
+    }
 }
