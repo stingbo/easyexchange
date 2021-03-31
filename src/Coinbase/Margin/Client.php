e01ebf58b38e2d34f8a99e68b648add5e6e65647
wanlianbo <lianbo.wan@gmail.com>
2021-03-31 22:20:03 +0800

    coinbase margin Get exit plan.

--- src/Coinbase/Margin/Client.php
+++ src/Coinbase/Margin/Client.php
@@ -63,4 +63,17 @@ class Client extends BaseClient
     {
         return $this->httpGet('/margin/withdrawal_power_all', [], 'SIGN');
     }
+
+    /**
+     * Get exit plan.
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function exitPlan()
+    {
+        return $this->httpGet('/margin/exit_plan', [], 'SIGN');
+    }
 }
