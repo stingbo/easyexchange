c639600456d47756ce3e3839f5241fb1161da271
sting <sting_bo@163.com>
2021-04-03 23:34:06 +0800

    gate spot trade Cancel a single order.

--- src/Gate/Spot/Client.php
+++ src/Gate/Spot/Client.php
@@ -335,4 +335,19 @@ class Client extends BaseClient
     {
         return $this->httpGet(sprintf('/api/v4/spot/price_orders/%s', $order_id), [], 'SIGN');
     }
+
+    /**
+     * Cancel a single order.
+     *
+     * @param $order_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function cancelPriceOrder($order_id)
+    {
+        return $this->httpDelete(sprintf('/api/v4/spot/price_orders/%s', $order_id), [], 'SIGN');
+    }
 }
