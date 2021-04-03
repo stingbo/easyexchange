0439301e94e984ebe3c87c34591356c98b48772a
sting <sting_bo@163.com>
2021-04-03 23:32:55 +0800

    gate spot trade Create a price-triggered order.

--- src/Gate/Spot/Client.php
+++ src/Gate/Spot/Client.php
@@ -274,4 +274,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v4/spot/my_trades', $params, 'SIGN');
     }
+
+    /**
+     * Create a price-triggered order.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function priceOrder($params)
+    {
+        return $this->httpPostJson('/api/v4/spot/price_orders', $params, [], 'SIGN');
+    }
 }
