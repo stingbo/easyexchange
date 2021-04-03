dccb83ccedf13c133e1c3e5c11cc9f04fcd64a62
sting <sting_bo@163.com>
2021-04-03 23:33:55 +0800

    gate spot trade Get a single order.

--- src/Gate/Spot/Client.php
+++ src/Gate/Spot/Client.php
@@ -320,4 +320,19 @@ class Client extends BaseClient
     {
         return $this->httpDelete('/api/v4/spot/price_orders', compact('market', 'account'), 'SIGN');
     }
+
+    /**
+     * Get a single order.
+     *
+     * @param $order_id
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getPriceOrder($order_id)
+    {
+        return $this->httpGet(sprintf('/api/v4/spot/price_orders/%s', $order_id), [], 'SIGN');
+    }
 }
