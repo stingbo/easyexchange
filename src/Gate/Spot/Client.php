e8c502ab0d272775a5ec61ef51141d73e3d03461
sting <sting_bo@163.com>
2021-04-03 23:33:14 +0800

    gate spot trade Retrieve running auto order list.

--- src/Gate/Spot/Client.php
+++ src/Gate/Spot/Client.php
@@ -289,4 +289,19 @@ class Client extends BaseClient
     {
         return $this->httpPostJson('/api/v4/spot/price_orders', $params, [], 'SIGN');
     }
+
+    /**
+     * Retrieve running auto order list.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function priceOrders($params)
+    {
+        return $this->httpGet('/api/v4/spot/price_orders', $params, 'SIGN');
+    }
 }
