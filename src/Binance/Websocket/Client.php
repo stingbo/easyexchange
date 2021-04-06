49ae61e7c4760ed470cc1c7e539a322fecd71849
sting <sting_bo@163.com>
2021-04-06 19:54:10 +0800

    optimize binance websocket client

--- src/Binance/Websocket/Client.php
+++ src/Binance/Websocket/Client.php
@@ -2,47 +2,18 @@
 
 namespace EasyExchange\Binance\Websocket;
 
-use EasyExchange\Binance\Kernel\BaseClient;
-use Workerman\Connection\AsyncTcpConnection;
-use Workerman\Worker;
+use EasyExchange\Kernel\WebsocketClient;
 
-class Client extends BaseClient
+class Client extends WebsocketClient
 {
     /**
      * Live Subscribing to streams.
      *
      * @param $params
      */
-    public function subscribe($params)
+    public function subscribe($params, callable $f)
     {
         $params['method'] = 'SUBSCRIBE';
-
-        $worker = new Worker();
-        // 进程启动时
-        $worker->onWorkerStart = function () use ($params) {
-            // 以websocket协议连接远程websocket服务器
-            $ws_connection = new AsyncTcpConnection('ws://stream.binance.com:9443/ws');
-            $ws_connection->transport = 'ssl';
-            // 连上后发送hello字符串
-            $ws_connection->onConnect = function ($connection) use ($params) {
-                echo 'connected'.PHP_EOL;
-                $connection->send(json_encode($params));
-            };
-            // 远程websocket服务器发来消息时
-            $ws_connection->onMessage = function ($connection, $data) {
-                echo "recv: $data\n";
-            };
-            // 连接上发生错误时，一般是连接远程websocket服务器失败错误
-            $ws_connection->onError = function ($connection, $code, $msg) {
-                echo "error: $msg\n";
-            };
-            // 当连接远程websocket服务器的连接断开时
-            $ws_connection->onClose = function ($connection) {
-                echo "connection closed\n";
-            };
-            // 设置好以上各种回调后，执行连接操作
-            $ws_connection->connect();
-        };
-        Worker::runAll();
+        $this->request('/ws', $params, $f);
     }
 }
