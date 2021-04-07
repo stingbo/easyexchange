ac68921c72f6b39aa0f28bef657a037ce13c79cb
wanlianbo <lianbo.wan@gmail.com>
2021-04-07 22:34:32 +0800

    update huobi websocket client extents

--- src/Huobi/Websocket/Client.php
+++ src/Huobi/Websocket/Client.php
@@ -2,17 +2,18 @@
 
 namespace EasyExchange\Huobi\Websocket;
 
-use EasyExchange\Kernel\WebsocketClient;
+use EasyExchange\Kernel\Websocket\BaseClient;
+use EasyExchange\Kernel\Websocket\DataHandle;
 
-class Client extends WebsocketClient
+class Client extends BaseClient
 {
     /**
      * Subscribe to Topic.
      *
      * @param $params
      */
-    public function subscribe($params, callable $f)
+    public function subscribe($params, DataHandle $handle)
     {
-        $this->request('/ws', $params, $f);
+        $this->request('/ws', $params, $handle);
     }
 }
