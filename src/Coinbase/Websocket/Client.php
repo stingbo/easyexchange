4f390f345b00a61bfdc083d2833e530b6a252931
sting <sting_bo@163.com>
2021-04-09 21:56:59 +0800

    add coinbase websocket Subscribe.

--- src/Coinbase/Websocket/Client.php
+++ src/Coinbase/Websocket/Client.php
@@ -3,7 +3,19 @@
 namespace EasyExchange\Coinbase\Websocket;
 
 use EasyExchange\Kernel\Websocket\BaseClient;
+use EasyExchange\Kernel\Websocket\DataHandle;
 
 class Client extends BaseClient
 {
+    /**
+     * Subscribe.
+     *
+     * @param $params
+     */
+    public function subscribe($params, DataHandle $handle)
+    {
+        $params['type'] = 'subscribe';
+
+        $this->request('', $params, $handle);
+    }
 }
