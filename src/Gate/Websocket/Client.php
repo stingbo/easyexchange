22806b41f7efefd87547cbac54b73b4af3bb1446
sting <sting_bo@163.com>
2021-04-09 10:13:34 +0800

    add gate websocket Subscribe.

--- src/Gate/Websocket/Client.php
+++ src/Gate/Websocket/Client.php
@@ -3,7 +3,19 @@
 namespace EasyExchange\Gate\Websocket;
 
 use EasyExchange\Kernel\Websocket\BaseClient;
+use EasyExchange\Kernel\Websocket\DataHandle;
 
 class Client extends BaseClient
 {
+    /**
+     * Subscribe.
+     *
+     * @param $params
+     */
+    public function subscribe($params, DataHandle $handle)
+    {
+        $params['event'] = 'subscribe';
+
+        $this->request('/ws/v4/', $params, $handle);
+    }
 }
