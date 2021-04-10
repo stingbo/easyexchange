b6fc82565e99e949c14219b92291165c2348fe7f
sting <sting_bo@163.com>
2021-04-10 10:18:34 +0800

    update kernel websocket baseclient,use handle to perform operations

--- src/Kernel/Websocket/BaseClient.php
+++ src/Kernel/Websocket/BaseClient.php
@@ -19,24 +19,24 @@ class BaseClient
     /**
      * @param $params
      */
-    public function request(string $url, $params, DataHandle $data_handle)
+    public function request(string $url, $params, Handle $handle)
     {
         $ws_base_uri = $this->app->config->get('ws_base_uri').$url;
         $worker = new Worker();
-        $worker->onWorkerStart = function () use ($ws_base_uri, $params, $data_handle) {
+        $worker->onWorkerStart = function () use ($ws_base_uri, $params, $handle) {
             $ws_connection = new AsyncTcpConnection($ws_base_uri);
             $ws_connection->transport = 'ssl';
-            $ws_connection->onConnect = function ($connection) use ($params) {
-                $connection->send(json_encode($params));
+            $ws_connection->onConnect = function ($connection) use ($params, $handle) {
+                $handle->onConnect($connection, $params);
             };
-            $ws_connection->onMessage = function ($connection, $data) use ($data_handle) {
-                $data_handle->handle($connection, $data);
+            $ws_connection->onMessage = function ($connection, $data) use ($handle) {
+                $handle->onMessage($connection, $data);
             };
-            $ws_connection->onError = function ($connection, $code, $msg) {
-                echo "error: $msg\n";
+            $ws_connection->onError = function ($connection, $code, $msg) use ($handle) {
+                $handle->onError($connection, $code, $msg);
             };
-            $ws_connection->onClose = function ($connection) {
-                echo "connection closed\n";
+            $ws_connection->onClose = function ($connection) use ($handle) {
+                $handle->onClose($connection);
             };
             $ws_connection->connect();
         };
