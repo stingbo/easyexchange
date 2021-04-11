b39647713d5ef7ce6d249badfb70846e59021460
sting <sting_bo@163.com>
2021-04-11 17:46:43 +0800

    add bittrex markets - List markets.

--- src/Bittrex/Market/Client.php
+++ src/Bittrex/Market/Client.php
@@ -6,4 +6,16 @@ use EasyExchange\Bittrex\Kernel\BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * markets - List markets.
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function markets()
+    {
+        return $this->httpGet('/v3/markets');
+    }
 }
