70e56cbbc2c36da2465eacb3b211e9202700ca80
sting <sting_bo@163.com>
2021-04-11 10:24:58 +0800

    update binance wallet test case

--- src/Binance/Wallet/Client.php
+++ src/Binance/Wallet/Client.php
@@ -25,14 +25,16 @@ class Client extends BaseClient
      * 获取所有币信息.
      * No sapi/wapi in testnet; only api endpoints available.
      *
+     * @param int $recvWindow
+     *
      * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
      *
      * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getAll()
+    public function getAll($recvWindow = 60000)
     {
-        return $this->httpGet('/sapi/v1/capital/config/getall', [], 'SIGN');
+        return $this->httpGet('/sapi/v1/capital/config/getall', compact('recvWindow'), 'SIGN');
     }
 
     /**
--- tests/Binance/Wallet/ClientTest.php
+++ tests/Binance/Wallet/ClientTest.php
@@ -10,9 +10,134 @@ class ClientTest extends TestCase
     public function testAccount()
     {
         $client = $this->mockApiClient(Client::class);
+        $recvWindow = 50000;
+        $params = [
+            'recvWindow' => $recvWindow,
+        ];
 
-        $client->expects()->httpGet('/api/v3/account', ['recvWindow' => 123], 'SIGN')->andReturn('mock-result');
+        $client->expects()->httpGet('/api/v3/account', $params, 'SIGN')->andReturn('mock-result');
 
-        $this->assertSame('mock-result', $client->account(123));
+        $this->assertSame('mock-result', $client->account($recvWindow));
+    }
+
+    public function testGetAll()
+    {
+        $client = $this->mockApiClient(Client::class);
+        $recvWindow = 50000;
+        $params = [
+            'recvWindow' => $recvWindow,
+        ];
+
+        $client->expects()->httpGet('/sapi/v1/capital/config/getall', $params, 'SIGN')->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->getAll($recvWindow));
+    }
+
+    public function testAccountSnapshot()
+    {
+        $client = $this->mockApiClient(Client::class);
+        $recvWindow = 50000;
+        $params = [
+            'type' => 'SPOT',
+            'recvWindow' => $recvWindow,
+        ];
+
+        $client->expects()->httpGet('/sapi/v1/accountSnapshot', $params, 'SIGN')->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->accountSnapshot($params));
+    }
+
+    public function testDisableFastWithdrawSwitch()
+    {
+        $client = $this->mockApiClient(Client::class);
+        $recvWindow = 50000;
+        $params = [
+            'recvWindow' => $recvWindow,
+        ];
+
+        $client->expects()->httpPost('/sapi/v1/account/disableFastWithdrawSwitch', $params, 'SIGN')->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->disableFastWithdrawSwitch($recvWindow));
+    }
+
+    public function testEnableFastWithdrawSwitch()
+    {
+        $client = $this->mockApiClient(Client::class);
+        $recvWindow = 50000;
+        $params = [
+            'recvWindow' => $recvWindow,
+        ];
+
+        $client->expects()->httpPost('/sapi/v1/account/enableFastWithdrawSwitch', $params, 'SIGN')->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->enableFastWithdrawSwitch($recvWindow));
+    }
+
+    public function testWithdrawApply()
+    {
+    }
+
+    public function testWithdraw()
+    {
+    }
+
+    public function testCapitalDepositHistory()
+    {
+    }
+
+    public function testDepositHistory()
+    {
+    }
+
+    public function testCapitalWithdrawHistory()
+    {
+    }
+
+    public function testWithdrawHistory()
+    {
+    }
+
+    public function testCapitalDepositAddress()
+    {
+    }
+
+    public function testDepositAddress()
+    {
+    }
+
+    public function testAccountStatus()
+    {
+    }
+
+    public function testApiTradingStatus()
+    {
+    }
+
+    public function testUserAssetDribbletLog()
+    {
+    }
+
+    public function testAssetDust()
+    {
+    }
+
+    public function testAssetDividend()
+    {
+    }
+
+    public function testAssetDetail()
+    {
+    }
+
+    public function testTradeFee()
+    {
+    }
+
+    public function testTransfer()
+    {
+    }
+
+    public function testTransferHistory()
+    {
     }
 }
