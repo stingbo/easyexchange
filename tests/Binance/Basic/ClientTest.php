5fda5620fe44150dfca72f9c0bc94678c2800df6
sting <sting_bo@163.com>
2021-04-13 09:08:52 +0800

    add binance basic test case

--- tests/Binance/Basic/ClientTest.php
+++ tests/Binance/Basic/ClientTest.php
@@ -15,4 +15,31 @@ class ClientTest extends TestCase
 
         $this->assertSame('mock-result', $client->ping());
     }
+
+    public function testSystemTime()
+    {
+        $client = $this->mockApiClient(Client::class);
+
+        $client->expects()->httpGet('/api/v3/time')->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->systemTime());
+    }
+
+    public function testExchangeInfo()
+    {
+        $client = $this->mockApiClient(Client::class);
+
+        $client->expects()->httpGet('/api/v3/exchangeInfo')->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->exchangeInfo());
+    }
+
+    public function testSystemStatus()
+    {
+        $client = $this->mockApiClient(Client::class);
+
+        $client->expects()->httpGet('/wapi/v3/systemStatus.html')->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->systemStatus());
+    }
 }
