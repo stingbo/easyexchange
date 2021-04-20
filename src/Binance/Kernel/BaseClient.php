054466c583f8a967aa2ea41968293d846a47ca9d
sting <sting_bo@163.com>
2021-04-20 20:30:12 +0800

    binance use proxyMiddleware

--- src/Binance/Kernel/BaseClient.php
+++ src/Binance/Kernel/BaseClient.php
@@ -44,6 +44,9 @@ class BaseClient extends \EasyExchange\Kernel\BaseClient
 
         // log
         $this->pushMiddleware($this->logMiddleware(), 'log');
+
+        // proxy
+        $this->pushMiddleware($this->proxyMiddleware(), 'proxy');
     }
 
     /**
