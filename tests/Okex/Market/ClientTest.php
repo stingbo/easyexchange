87abc4febe3b2d2d2882aac63bcae69a4c8d8d60
sting <sting_bo@163.com>
2021-04-21 09:06:48 +0800

    update okex market test case

--- tests/Okex/Market/ClientTest.php
+++ tests/Okex/Market/ClientTest.php
@@ -7,6 +7,21 @@ use EasyExchange\Tests\TestCase;
 
 class ClientTest extends TestCase
 {
+    public function testTickers()
+    {
+        $client = $this->mockApiClient(Client::class);
+        $instType = 'SWAP';
+        $uly = 'BTC-USD';
+        $params = [
+            'instType' => $instType,
+            'uly' => $uly,
+        ];
+
+        $client->expects()->httpGet('/api/v5/market/tickers', $params)->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->tickers($instType, $uly));
+    }
+
     public function testDepth()
     {
         $client = $this->mockApiClient(Client::class);
