cbfdd503f2ddebd1acfc8e231d8f459da568ee88
sting <sting_bo@163.com>
2021-05-05 00:17:00 +0800

    okex websocket handle save message

--- src/Okex/Websocket/Handle.php
+++ src/Okex/Websocket/Handle.php
@@ -12,7 +12,7 @@ class Handle implements \EasyExchange\Kernel\Websocket\Handle
     {
         $this->config = $config;
 
-        $ws_base_uri = $config['websocket']['base_uri'].'/ws/v5/public';
+        $ws_base_uri = $config['base_uri'].'/ws/v5/public';
 //        $ws_base_uri = 'ws://ws.okex.com:8443/ws/v5/public?brokerId=9999';
         echo $ws_base_uri.PHP_EOL;
 
@@ -22,24 +22,37 @@ class Handle implements \EasyExchange\Kernel\Websocket\Handle
         return $connection;
     }
 
-    public function onConnect($connection, $params)
+    public function onConnect($connection, $client, $params)
     {
         echo 'connect:-----------------'.PHP_EOL;
         $connection->send(json_encode($params));
     }
 
-    public function onMessage($connection, $params, $data)
+    public function onMessage($connection, $client, $params, $data)
     {
         echo 'msg:------------------';
         echo $data.PHP_EOL;
+        $old_value = $client->okex_data ?? [];
+        if (!$old_value) {
+            $client->add('okex_data', [$data]);
+        } else {
+            $max_size = $this->config['max_size'] ?? 100;
+            do {
+                $new_value = $old_value;
+                array_unshift($new_value, $data);
+                if (count($new_value) > $max_size) {
+                    $new_value = array_slice($new_value, 0, $max_size);
+                }
+            } while (!$client->cas('okex_data', $old_value, $new_value));
+        }
     }
 
-    public function onError($connection, $code, $message)
+    public function onError($connection, $client, $code, $message)
     {
         echo "error: $message\n";
     }
 
-    public function onClose($connection)
+    public function onClose($connection, $client)
     {
         echo "connection closed\n";
     }
