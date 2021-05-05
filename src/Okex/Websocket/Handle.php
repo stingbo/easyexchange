1e89c0238d7c81dd83111cd5ba0b6ed5a0a39864
sting <sting_bo@163.com>
2021-05-05 22:08:14 +0800

    Optimize the okex websocket handle

--- src/Okex/Websocket/Handle.php
+++ src/Okex/Websocket/Handle.php
@@ -12,7 +12,11 @@ class Handle implements \EasyExchange\Kernel\Websocket\Handle
     {
         $this->config = $config;
 
-        $ws_base_uri = $config['websocket']['base_uri'].'/ws/v5/public';
+        if (isset($params['private']) && $params['private']) {
+            $ws_base_uri = $config['websocket']['base_uri'].'/ws/v5/private';
+        } else {
+            $ws_base_uri = $config['websocket']['base_uri'].'/ws/v5/public';
+        }
         echo $ws_base_uri.PHP_EOL;
 
         $connection = new AsyncTcpConnection($ws_base_uri);
@@ -29,34 +33,58 @@ class Handle implements \EasyExchange\Kernel\Websocket\Handle
 
     public function onMessage($connection, $client, $params, $data)
     {
-        echo 'msg:------------------';
+        echo 'msg:--------------------------------'.PHP_EOL;
         echo $data.PHP_EOL;
         if ('pong' == $data) {
             return true;
         }
 
         // save login result
-        $result = josn_decode($data, true) ?? [];
-        if (isset($result['event']) && 'login' == $result['event'] && 0 == $result['code']) {
-            $client->okex_is_auth = 1;
+        $result = json_decode($data, true) ?? [];
+        if (isset($result['event'])) {
+            if ('login' == $result['event'] && 0 == $result['code']) {
+                $client->okex_is_auth = 1;
+            } elseif ('subscribe' == $result['event']) {
+                $old_subs = $client->okex_sub_old ?? [];
+                if (!$old_subs) {
+                    $client->okex_sub_old = ['op' => 'subscribe', 'args' => [$result['arg']]];
+                } else {
+                    if (!in_array($result['arg'], $old_subs['args'])) {
+                        do {
+                            $new_subs = $old_subs = $client->okex_sub_old;
+                            $new_subs['args'][] = $result['arg'];
+                        } while (!$client->cas('okex_sub_old', $old_subs, $new_subs));
+                    }
+                }
+            }
         }
 
-        $old_value = $client->okex_data ?? [];
-        if (!$old_value) {
-            $client->add('okex_data', [$data]);
-        } else {
-            $max_size = $this->config['websocket']['max_size'] ?? 100;
-            $max_size = ($max_size > 1000 || $max_size <= 0) ? 100 : $max_size;
-            do {
-                $new_value = $old_value;
-                if (count($new_value) >= $max_size) {
-                    array_unshift($new_value, $data);
-                    array_pop($new_value);
-                } else {
-                    array_unshift($new_value, $data);
-                }
-            } while (!$client->cas('okex_data', $old_value, $new_value));
+        // save data by channel
+        if ($result && is_array($result) && isset($result['arg']) && isset($result['data'])) {
+            $channel = $result['arg']['channel'] ?? '';
+            if (!$channel) {
+                return true;
+            }
+            $key = 'okex_list_'.$channel;
+            $old_list = $client->{$key} ?? [];
+            if (!$old_list) {
+                $client->add($key, [$data]);
+            } else {
+                $max_size = $this->config['websocket']['max_size'] ?? 100;
+                $max_size = ($max_size > 1000 || $max_size <= 0) ? 100 : $max_size;
+                do {
+                    $new_list = $old_list = $client->{$key};
+                    if (count($new_list) >= $max_size) {
+                        array_unshift($new_list, $data);
+                        array_pop($new_list);
+                    } else {
+                        array_unshift($new_list, $data);
+                    }
+                } while (!$client->cas($key, $old_list, $new_list));
+            }
         }
+
+        return true;
     }
 
     public function onError($connection, $client, $code, $message)
@@ -66,46 +94,26 @@ class Handle implements \EasyExchange\Kernel\Websocket\Handle
 
     public function onClose($connection, $client)
     {
-        echo "connection closed\n";
-    }
+        echo "connection closed，now reconnect\n";
+        $connection->reConnect(1);
 
-    /**
-     * login.
-     *
-     * @param $connection
-     */
-    public function login($connection)
-    {
-        $timestamp = time();
-        $sign = $this->getSignature($timestamp);
-        $params = [
-            'op' => 'login',
-            'args' => [
-                [
-                    'apiKey' => $this->config['app_key'],
-                    'passphrase' => $this->config['passphrase'],
-                    'timestamp' => $timestamp,
-                    'sign' => $sign,
-                ],
-            ],
-        ];
-        $connection->send(json_encode($params));
-    }
+        $public = [];
+        $private = [];
+        $subs = $client->okex_sub_old;
+        foreach ($subs['args'] as $arg) {
+            if (in_array($arg['channel'], $this->config['auth_channel'])) {
+                $private[] = $arg;
+            } else {
+                $public[] = $arg;
+            }
+        }
+        $client->okex_sub_old = [];
 
-    /**
-     * get sign.
-     *
-     * @param $timestamp
-     * @param string $method
-     * @param string $uri_path
-     *
-     * @return string
-     */
-    public function getSignature($timestamp, $method = 'GET', $uri_path = '/users/self/verify')
-    {
-        $message = (string) $timestamp.$method.$uri_path;
-        $secret = $this->config['secret'];
-
-        return base64_encode(hash_hmac('sha256', $message, $secret, true));
+        if ($public) {
+            $client->okex_sub = ['op' => 'subscribe', 'args' => $public];
+        }
+        if ($private) {
+            $client->okex_sub_private = ['op' => 'subscribe', 'args' => $private];
+        }
     }
 }
