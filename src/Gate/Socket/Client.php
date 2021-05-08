7f16192b0ce92991f6c1c06a6ace3bcea05c4e06
sting <sting_bo@163.com>
2021-05-08 18:50:56 +0800

    update gate socket client

--- src/Gate/Socket/Client.php
+++ src/Gate/Socket/Client.php
@@ -2,20 +2,218 @@
 
 namespace EasyExchange\Gate\Socket;
 
+use EasyExchange\Kernel\Exceptions\InvalidArgumentException;
 use EasyExchange\Kernel\Socket\BaseClient;
-use EasyExchange\Kernel\Socket\Handle;
+use EasyExchange\Kernel\Support\Arr;
+use Workerman\Timer;
+use Workerman\Worker;
 
 class Client extends BaseClient
 {
+    public $client_type = 'gate';
+
+    public function getClientType(): string
+    {
+        return $this->client_type;
+    }
+
     /**
-     * Subscribe.
+     * Subscribe to a stream.
      *
      * @param $params
+     *
+     * @throws InvalidArgumentException
      */
-    public function subscribe($params, Handle $handle)
+    public function subscribe($params)
     {
-        $params['event'] = 'subscribe';
+        if ('subscribe' != $params['event']) {
+            throw new InvalidArgumentException('Invalid argument about event type');
+        }
+        $this->updateOrCreate($this->client_type.'_sub', $params);
+    }
 
-        $this->request($params, $handle);
+    /**
+     * Unsubscribe to a stream.
+     *
+     * @param $params
+     *
+     * @throws InvalidArgumentException
+     */
+    public function unsubscribe($params)
+    {
+        if ('unsubscribe' != $params['event']) {
+            throw new InvalidArgumentException('Invalid argument about event type');
+        }
+
+        $this->updateOrCreate($this->client_type.'_unsub', $params);
+    }
+
+    /**
+     * Get subscribed channels.
+     *
+     * @return array|mixed
+     */
+    public function getSubChannel()
+    {
+        return $this->get($this->client_type.'_sub_old');
+    }
+
+    /**
+     * Get Data by Channel.
+     *
+     * @param array $channels [channel_name ...], default all
+     * @param false $daemon
+     * @param null  $callback
+     *
+     * @return array
+     */
+    public function getChannelData($channels = [], $callback = null, $daemon = false)
+    {
+        if (!$channels) {
+            $subs = $this->get($this->client_type.'_sub_old');
+            if ($subs) {
+                $channels = array_unique(array_column($subs['params'] ?? [], 'channel'));
+            }
+        }
+
+        if ($daemon) {
+            $this->getDataWithDaemon($channels, $callback);
+        }
+
+        return $this->getData($channels, $callback);
+    }
+
+    /**
+     * @param array $channels subscribe channel
+     * @param null  $callback callback
+     */
+    public function getDataWithDaemon($channels = [], $callback = null)
+    {
+        $worker = new Worker();
+        $worker->onWorkerStart = function () use ($callback, $channels) {
+            $time = $this->config['websocket']['data_time'] ?? 0.1;
+
+            Timer::add($time, function () use ($channels, $callback) {
+                $this->getData($channels, $callback);
+            });
+        };
+        Worker::runAll();
+    }
+
+    /**
+     * Get data.
+     *
+     * @param array $channels subscribe channel
+     * @param null  $callback callback
+     *
+     * @return array
+     */
+    protected function getData($channels = [], $callback = null)
+    {
+        $data = [];
+        foreach ($channels as $channel) {
+            $key = $this->client_type.'_list_'.$channel;
+            $data[$channel] = $this->get($key);
+
+            if (null !== $callback) {
+                call_user_func_array($callback, [$data[$channel]]);
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * Heartbeat.
+     *
+     * @param $connection
+     */
+    public function ping($connection)
+    {
+        return true;
+    }
+
+    /**
+     * Communicate with the server.
+     *
+     * @param $connection
+     */
+    public function connect($connection)
+    {
+        $interval = $this->config['websocket']['timer_time'] ?? 3;
+        $connection->timer_id = Timer::add($interval, function () use ($connection) {
+            echo 'public:-------------------'.PHP_EOL;
+            // subscribe
+            $this->subPublic($connection);
+
+            // unsubscribe
+            $this->unSubPublic($connection);
+        });
+    }
+
+    /**
+     * subscribe.
+     *
+     * @param $connection
+     *
+     * @return bool
+     */
+    public function subPublic($connection)
+    {
+        $subs = $this->get($this->client_type.'_sub');
+        print_r($subs);
+        if (!$subs) {
+            return true;
+        } else {
+            if (!isset($subs['channel']) || !$subs['channel']) {
+                return true;
+            }
+
+            // check if this channel is subscribed
+            $old_subs = $this->get($this->client_type.'_sub_old');
+            $channels = array_column($old_subs, 'channel');
+            if (in_array($subs['channel'], $channels)) {
+                $this->delete($this->client_type.'_sub');
+
+                return true;
+            }
+
+            $connection->send(json_encode($subs));
+            $this->delete($this->client_type.'_sub');
+        }
+
+        return true;
+    }
+
+    /**
+     * cancel subscribe.
+     *
+     * @param $connection
+     *
+     * @return bool
+     */
+    public function unSubPublic($connection)
+    {
+        $unsubs = $this->get($this->client_type.'_unsub');
+        if (!$unsubs) {
+            return true;
+        } else {
+            $old_subs = $this->get($this->client_type.'_sub_old');
+            if (!$old_subs) {
+                return true;
+            }
+
+            $connection->send(json_encode($unsubs));
+            $this->delete($this->client_type.'_unsub');
+
+            $old_subs = Arr::diff($old_subs, $unsubs);
+            if ($old_subs) {
+                $this->updateOrCreate($this->client_type.'_sub_old', $old_subs);
+            } else {
+                $this->updateOrCreate($this->client_type.'_sub_old', []);
+            }
+        }
+
+        return true;
     }
 }
