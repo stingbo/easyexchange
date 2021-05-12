03fd8f528c08883efafaee54b12151c6e740b656
sting <sting_bo@163.com>
2021-05-12 09:07:10 +0800

    update huobi socket handle about save data

--- src/Huobi/Socket/Handle.php
+++ src/Huobi/Socket/Handle.php
@@ -22,6 +22,7 @@ class Handle implements \EasyExchange\Kernel\Socket\Handle
 
     public function onConnect($connection, $client, $params)
     {
+        echo 'connect huobi:-----------------'.PHP_EOL;
         $connection->send(json_encode($params));
     }
 
@@ -29,9 +30,44 @@ class Handle implements \EasyExchange\Kernel\Socket\Handle
     {
         $json_data = gzdecode($data);
         echo $json_data.PHP_EOL;
-        $data = json_decode($json_data, true);
-        if (isset($data['ping'])) {
-            $connection->send(json_encode(['pong' => $data['ping']]));
+        $result = json_decode($json_data, true);
+        if (isset($result['ping'])) {
+            $connection->send(json_encode(['pong' => $result['ping']]));
+        }
+
+        if (isset($result['subbed']) && isset($result['status']) && 'ok' == $result['status']) {
+            $old_subs = $client->huobi_sub_old ?? [];
+            if (!$old_subs) {
+                $client->huobi_sub_old = ['id' => $result['id'], 'sub' => $result['subbed']];
+            } else {
+                $channels = array_column($old_subs, 'sub');
+                if (!in_array($result['subbed'], $channels)) {
+                    do {
+                        $new_subs = $old_subs = $client->huobi_sub_old;
+                        $new_subs[] = ['id' => $result['id'], 'sub' => $result['subbed']];
+                    } while (!$client->cas('huobi_sub_old', $old_subs, $new_subs));
+                }
+            }
+        }
+
+        if (isset($result['ch'])) {
+            $key = 'huobi_list_'.$result['ch'];
+            $old_list = $client->{$key} ?? [];
+            if (!$old_list) {
+                $client->add($key, [$json_data]);
+            } else {
+                $max_size = $this->config['websocket']['max_size'] ?? 100;
+                $max_size = ($max_size > 1000 || $max_size <= 0) ? 100 : $max_size;
+                do {
+                    $new_list = $old_list = $client->{$key};
+                    if (count($new_list) >= $max_size) {
+                        array_unshift($new_list, $json_data);
+                        array_pop($new_list);
+                    } else {
+                        array_unshift($new_list, $json_data);
+                    }
+                } while (!$client->cas($key, $old_list, $new_list));
+            }
         }
     }
 
