e20d4ac31a52cb82087b556a68c1d2b62082666c
sting <sting_bo@163.com>
2021-05-15 23:04:22 +0800

    update coinbase socket handle onmessage

--- src/Coinbase/Socket/Handle.php
+++ src/Coinbase/Socket/Handle.php
@@ -30,6 +30,25 @@ class Handle implements \EasyExchange\Kernel\Socket\Handle
     public function onMessage($connection, $client, $params, $data)
     {
         echo $data.PHP_EOL;
+        $result = json_decode($data, true) ?? [];
+        if (isset($result['type']) && 'subscriptions' == $result['type']) {
+            $old_subs = $client->coinbase_sub_old ?? [];
+            if (!$old_subs) {
+                $client->coinbase_sub_old = $result;
+            } else {
+                foreach ($old_subs['channels'] as &$old_sub) {
+                    foreach ($result['channels'] as $new_sub) {
+                        if ($old_sub['name'] == $new_sub['name']) {
+                            $old_sub['product_ids'] = array_unique(array_filter(array_merge($old_sub['product_ids'], $new_sub['product_ids'])));
+                        }
+                    }
+                }
+                $new_subs = $old_subs;
+                do {
+                    $old_subs = $client->coinbase_sub_old;
+                } while (!$client->cas('coinbase_sub_old', $old_subs, $new_subs));
+            }
+        }
     }
 
     public function onError($connection, $client, $code, $message)
