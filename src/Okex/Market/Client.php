0fd4b84c29c9fab9c0e42d0d40531d2ffddf1cef
sting <sting_bo@163.com>
2022-01-19 12:50:13 +0800

    https://www.okx.com/docs-v5/en/#rest-api-market-data-get-exchange-rate

--- src/Okex/Market/Client.php
+++ src/Okex/Market/Client.php
@@ -144,4 +144,15 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/market/trades', compact('instId', 'limit'));
     }
+
+    /**
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function exchangeRate()
+    {
+        return $this->httpGet('/api/v5/market/exchange-rate');
+    }
 }
