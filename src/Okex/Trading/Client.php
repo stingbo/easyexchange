3645149b2e4689bc78475f56560fac52db018a93
sting <sting_bo@163.com>
2022-01-21 23:49:19 +0800

    https://www.okx.com/docs-v5/en/#rest-api-trading-data-get-taker-volume

--- src/Okex/Trading/Client.php
+++ src/Okex/Trading/Client.php
@@ -18,4 +18,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/rubik/stat/trading-data/support-coin');
     }
+
+    /**
+     * Get taker volume.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function takerVolume($params)
+    {
+        return $this->httpGet('/api/v5/rubik/stat/taker-volume', $params);
+    }
 }
