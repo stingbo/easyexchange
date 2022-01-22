d9e276f81dbbffc4a1dc1356e0cdd168ea38de29
sting <sting_bo@163.com>
2022-01-22 09:48:04 +0800

    https://www.okx.com/docs-v5/en/#rest-api-trading-data-get-options-open-interest-and-volume

--- src/Okex/Trading/Client.php
+++ src/Okex/Trading/Client.php
@@ -78,4 +78,19 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/rubik/stat/contracts/open-interest-volume', $params);
     }
+
+    /**
+     * Get options open interest and volume.
+     *
+     * @param $params
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function optionOpenInterestVolume($params)
+    {
+        return $this->httpGet('/api/v5/rubik/stat/option/open-interest-volume', $params);
+    }
 }
