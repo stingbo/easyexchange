eac11f1bc658a2ab2e661c667cc618eba4443d9a
sting <sting_bo@163.com>
2022-01-22 10:04:19 +0800

    https://www.okx.com/docs-v5/en/#rest-api-trading-data-get-taker-flow

--- src/Okex/Trading/Client.php
+++ src/Okex/Trading/Client.php
@@ -142,4 +142,20 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/v5/rubik/stat/option/open-interest-volume-strike', compact('ccy', 'expTime', 'period'));
     }
+
+    /**
+     * Get taker flow.
+     *
+     * @param $ccy
+     * @param string $period
+     *
+     * @return array|\EasyExchange\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyExchange\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function optionTakerBlockVolume($ccy, $period = '8H')
+    {
+        return $this->httpGet('/api/v5/rubik/stat/option/taker-block-volume', compact('ccy', 'period'));
+    }
 }
